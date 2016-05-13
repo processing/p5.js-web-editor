33fcafb483c13b4c9d8b6bc3866cad57f2f3c4c5
catarak <ctarakajian@gmail.com>
2016-05-13 16:49:56 -0400

    add framework for sessions

--- server/routes/session.routes.js
+++ server/routes/session.routes.js
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 import * as SessionController from '../controllers/session.controller';
+<<<<<<< HEAD
 import passport from 'passport';
 
+=======
+>>>>>>> add framework for sessions
 const router = new Router();
 
 router.route('/login').get(SessionController.newSession);
 
+<<<<<<< HEAD
 router.route('/login').post(SessionController.createSession);
 
 router.route('/logout').get(SessionController.destroySession);
 
 //TODO add github authentication stuff
+=======
+router.route('/logout').get(SessionController.destroySession);
+>>>>>>> add framework for sessions
