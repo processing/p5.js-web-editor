ba1cf0c6446a22ebcd9bc9a08d782408f94c01bf
catarak <ctarakajian@gmail.com>
2016-05-18 13:37:59 -0400

    add react router

--- server/routes/session.routes.js
+++ server/routes/session.routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import * as SessionController from '../controllers/session.controller';
 import passport from 'passport';
+<<<<<<< HEAD
 
+=======
+>>>>>>> add react router
 const router = new Router();
 
 router.route('/login').get(SessionController.newSession);
@@ -10,4 +13,8 @@ router.route('/login').post(SessionController.createSession);
 
 router.route('/logout').get(SessionController.destroySession);
 
+<<<<<<< HEAD
 //TODO add github authentication stuff
+=======
+//TODO add github authentication stuff
+>>>>>>> add react router
