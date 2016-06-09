75b33a18c252cc8f61d9f61111803b29bbe1639f
catarak <ctarakajian@gmail.com>
2016-06-09 18:41:40 -0400

    add signup, not functional authentication yet though

--- shared/redux/actions/user.js
+++ shared/redux/actions/user.js
@@ -6,15 +6,21 @@ import axios from 'axios'
 const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:8000' : '/';
 
 export function signUpUser(formValues) {
-  const request = axios.post(`${ROOT_URL}/signup`, formValues);
-
   return function(dispatch) {
     axios.post(`${ROOT_URL}/signup`, formValues)
       .then(response => {
-        dispatch({ type: AUTH_USER });
+        dispatch({ type: ActionTypes.AUTH_USER });
         localStorage.setItem('token', response.data.token);
         browserHistory.push('/');
       })
       .catch(response => dispatch(authError(response.data.error)));
   }
+}
+
+
+export function authError(error) {
+  return {
+    type: ActionTypes.AUTH_ERROR,
+    payload: error
+  };
 }
--- shared/redux/store/configureStore.js
+++ shared/redux/store/configureStore.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
-    initialState
-    // applyMiddleware(thunk)
+    initialState,
+    applyMiddleware(thunk)
   )
 
   if (module.hot) {
