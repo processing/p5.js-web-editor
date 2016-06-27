29013b99f1c676ab61092f44afe87fd09410653b
catarak <ctarakajian@gmail.com>
2016-06-27 14:00:45 -0400

    add proptypes to login form

--- client/modules/User/components/LoginForm.js
+++ client/modules/User/components/LoginForm.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-class LoginForm extends React.Component {
-  render() {
-    const { fields: { email, password }, handleSubmit } = this.props;
-    return (
-      <form className="login-form" onSubmit={handleSubmit(this.props.loginUser.bind(this))}>
-        <p className="login-form__field">
-          <label className="login-form__email-label" htmlFor="email">Email:</label>
-          <input
-            className="login-form__email-input"
-            id="email"
-            type="text"
-            placeholder="Email"
-            {...email}
-          />
-        </p>
-        <p className="login-form__field">
-          <label className="signup-form__password-label" htmlFor="password">Password:</label>
-          <input
-            className="signup-form__password-input"
-            id="password"
-            type="password"
-            placeholder="Password"
-            {...password}
-          />
-        </p>
-        <input type="submit" value="Login" />
-      </form>
-    );
-  }
+function LoginForm(props) {
+  const { fields: { email, password }, handleSubmit } = props;
+  return (
+    <form className="login-form" onSubmit={handleSubmit(props.loginUser.bind(this))}>
+      <p className="login-form__field">
+        <label className="login-form__email-label" htmlFor="email">Email:</label>
+        <input
+          className="login-form__email-input"
+          id="email"
+          type="text"
+          placeholder="Email"
+          {...email}
+        />
+      </p>
+      <p className="login-form__field">
+        <label className="signup-form__password-label" htmlFor="password">Password:</label>
+        <input
+          className="signup-form__password-input"
+          id="password"
+          type="password"
+          placeholder="Password"
+          {...password}
+        />
+      </p>
+      <input type="submit" value="Login" />
+    </form>
+  );
 }
+
+LoginForm.propTypes = {
+  fields: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+  }).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  loginUser: PropTypes.func.isRequired
+};
 
 export default LoginForm;
