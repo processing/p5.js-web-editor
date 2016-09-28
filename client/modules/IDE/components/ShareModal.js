46d58014ff2b96e9fc659316192ddc1f77e12d00
MathuraMG <mmg9291@gmail.com>
2016-09-28 12:09:42 -0400

    add focus and labels

--- client/modules/IDE/components/ShareModal.js
+++ client/modules/IDE/components/ShareModal.js
@@ -2,42 +2,47 @@ import React, { PropTypes } from 'react';
 import InlineSVG from 'react-inlinesvg';
 const exitUrl = require('../../../images/exit.svg');
 
-function ShareModal(props) {
-  const hostname = window.location.origin;
-  return (
-    <section className="share-modal">
-      <header className="share-modal__header">
-        <h2>Share Sketch</h2>
-        <button className="about__exit-button" onClick={props.closeShareModal}>
-          <InlineSVG src={exitUrl} alt="Close Share Overlay" />
-        </button>
-      </header>
-      <div className="share-modal__section">
-        <label className="share-modal__label">Embed</label>
-        <input
-          type="text"
-          className="share-modal__input"
-          value={`<iframe src="${hostname}/embed/${props.projectId}"></iframe>`}
-        />
-      </div>
-      <div className="share-modal__section">
-        <label className="share-modal__label">Fullscreen</label>
-        <input
-          type="text"
-          className="share-modal__input"
-          value={`${hostname}/full/${props.projectId}`}
-        />
-      </div>
-      <div className="share-modal__section">
-        <label className="share-modal__label">Edit</label>
-        <input
-          type="text"
-          className="share-modal__input"
-          value={`${hostname}/projects/${props.projectId}`}
-        />
-      </div>
-    </section>
-  );
+class ShareModal extends React.Component {
+  componentDidMount() {
+    this.refs.shareModal.focus();
+  }
+  render() {
+    const hostname = window.location.origin;
+    return (
+      <section className="share-modal" ref="shareModal" tabIndex="0">
+        <header className="share-modal__header">
+          <h2>Share Sketch</h2>
+          <button className="about__exit-button" onClick={this.props.closeShareModal}>
+            <InlineSVG src={exitUrl} alt="Close Share Overlay" />
+          </button>
+        </header>
+        <div className="share-modal__section">
+          <label className="share-modal__label">Embed</label>
+          <input
+            type="text"
+            className="share-modal__input"
+            value={`<iframe src="${hostname}/embed/${this.props.projectId}"></iframe>`}
+          />
+        </div>
+        <div className="share-modal__section">
+          <label className="share-modal__label">Fullscreen</label>
+          <input
+            type="text"
+            className="share-modal__input"
+            value={`${hostname}/full/${this.props.projectId}`}
+          />
+        </div>
+        <div className="share-modal__section">
+          <label className="share-modal__label">Edit</label>
+          <input
+            type="text"
+            className="share-modal__input"
+            value={`${hostname}/projects/${this.props.projectId}`}
+          />
+        </div>
+      </section>
+    );
+  }
 }
 
 ShareModal.propTypes = {
