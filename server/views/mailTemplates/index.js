a3e0b0ed4f98def455e866fb87ffd70ad46d6860
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2017-06-07 22:49:46 +0200

    Removes unused mail templates

--- server/views/mailTemplates/index.js
+++
@@ -1,7 +0,0 @@
-// import emailVerification from './email-verification';
-import resetPassword from './reset-password';
-
-export default {
-  // 'email-verification': emailVerification,
-  'reset-password': resetPassword,
-};
