2aed6bd59b3e637487b22bf30ea0b5e9eca927e5
Zach Rispoli <zach.rispoli@gmail.com>
2017-06-12 10:12:12 -0400

    start implementing collections API

--- server/controllers/collection.controller.js
+++ server/controllers/collection.controller.js
@@ -6,7 +6,32 @@ import Project from '../models/project';
 import User from '../models/user';
 
 export function createCollection(req, res) {
-	res.send('Workin on this...');
+  if (!req.user) {
+    res.status(403).send({ success: false, message: 'Session does not match owner of collection.' });
+    return;
+  }
+
+  let collectionValues = {
+    user: req.user._id
+  };
+
+  collectionValues = Object.assign(collectionValues, req.body);
+
+  Collection.create(collectionValues, (err, newProject) => {
+    if (err) {
+      res.json({ success: false });
+      return;
+    }
+    Collection.populate(newProject,
+      { path: 'user', select: 'username' },
+      (innerErr, newCollectionWithUser) => {
+        if (innerErr) {
+          res.json({ success: false });
+          return;
+        }
+        res.json(newCollectionWithUser);
+      });
+  });
 }
 
 export function updateCollection(req, res) {
--- server/models/classroom.js
+++ server/models/classroom.js
@@ -3,11 +3,11 @@ import shortid from 'shortid';
 
 const Schema = mongoose.Schema;
 
-const collectionSchema = new Schema({
-  name: { type: String, default: "New Collection" },
+const classroomSchema = new Schema({
+  name: { type: String, default: "New Classroom" },
   owners: { type: [Schema.Types.ObjectId] },
   members: { type: [Schema.Types.ObjectId] },
-  projects: { type: [Schema.Types.ObjectId] },
+  collections: { type: [Schema.Types.ObjectId] },
   isPrivate: { type: Boolean },
   _id: { type: String, default: shortid.generate }
 }, { timestamps: true });
@@ -16,4 +16,4 @@ collectionSchema.virtual('id').get(function getCollectionId() {
   return this._id;
 });
 
-export default mongoose.model('Collection', collectionSchema);
+export default mongoose.model('Classroom', collectionSchema);
