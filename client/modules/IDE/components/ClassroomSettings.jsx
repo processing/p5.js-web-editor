e5a589c15107b4402b4b6c9fe9f4b6864c748064
Zach Rispoli <zach.rispoli@gmail.com>
2017-08-27 18:13:08 -0400

    classroom settings styling

--- client/modules/IDE/components/ClassroomSettings.jsx
+++ client/modules/IDE/components/ClassroomSettings.jsx
@@ -1,144 +1,3 @@
-/* import React, { PropTypes } from 'react';
-import axios from 'axios';
-import { reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import moment from 'moment';
-import { Link, browserHistory } from 'react-router';
-import InlineSVG from 'react-inlinesvg';
-import * as ClassroomActions from '../actions/classroom';
-import * as ProjectActions from '../actions/project';
-import * as ToastActions from '../actions/toast';
-import { domOnlyProps } from '../../../utils/reduxFormUtils';
-
-const leftArrow = require('../../../images/left-arrow.svg');
-const exitUrl = require('../../../images/exit.svg');
-const trashCan = require('../../../images/trash-can.svg');
-
-function ClassroomOwnerSettingsForm(props) {
-  const {
-    fields: { name, description },
-    handleSubmit,
-    submitting,
-    invalid,
-    pristine
-  } = props;
-  return (
-    <form className="form" onSubmit={handleSubmit(props.updateClassroom)}>
-      <p className="form__field">
-        <label htmlFor="name" className="form__label">Name</label>
-        <input
-          className="form__input"
-          aria-label="name"
-          type="text"
-          id="name"
-          {...domOnlyProps(name)}
-        />
-      </p>
-      <p className="form__field">
-        <label htmlFor="description" className="form__label">Description</label>
-        <input
-          className="form__input"
-          aria-label="description"
-          type="text"
-          id="description"
-          {...domOnlyProps(description)}
-        />
-      </p>
-      <input type="submit" disabled={submitting || invalid || pristine} value="Save" aria-label="updateClassroom" />
-    </form>
-  );
-}
-
-ClassroomOwnerSettingsForm.propTypes = {
-  fields: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
-  }).isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  updateClassroom: PropTypes.func.isRequired,
-  submitting: PropTypes.bool,
-  invalid: PropTypes.bool,
-  pristine: PropTypes.bool,
-};
-
-ClassroomOwnerSettingsForm.defaultProps = {
-  submitting: false,
-  pristine: true,
-  invalid: false
-};
-
-class ClassroomOwnerSettings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.closeClassroomSettingsPage = this.closeClassroomSettingsPage.bind(this);
-    this.goBackToClassroom = this.goBackToClassroom.bind(this);
-  }
-
-  componentDidMount() {
-    document.getElementById('createclassroomfields').focus();
-  }
-
-  closeClassroomSettingsPage() {
-    // browserHistory.push(this.props.previousPath);
-    browserHistory.push('/');
-  }
-
-  goBackToClassroom() {
-    browserHistory.push(`/classroom/${this.props.classroom._id}`);
-  }
-
-  render() {
-    return (
-      <section className="sketch-list" aria-label="submissions list" tabIndex="0" role="main" id="createclassroomfields">
-        <header className="sketch-list__header">
-          <h2 className="sketch-list__header-title">Classroom Settings</h2>
-          <button className="sketch-list__exit-button" onClick={this.goBackToClassroom}>
-            <InlineSVG src={leftArrow} alt="Go Back To Classroom" />
-          </button>
-          <button className="sketch-list__exit-button" onClick={this.closeClassroomSettingsPage}>
-            <InlineSVG src={exitUrl} alt="Close Create Classroom Overlay" />
-          </button>
-        </header>
-        <div className="sketches-table-container">
-          <ClassroomOwnerSettingsForm {...this.props} />
-        </div>
-      </section>
-    );
-  }
-}
-
-ClassroomOwnerSettings.propTypes = {
-  // previousPath: PropTypes.string.isRequired,
-  classroom: {},
-};
-
-ClassroomOwnerSettings.defaultProps = {
-  classroom: {},
-};
-
-function mapStateToProps(state) {
-  return {
-    classroom: state.classroom,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Object.assign({}, ClassroomActions, ProjectActions, ToastActions), dispatch);
-}
-
-export default reduxForm({
-  form: 'updateAllSettings',
-  fields: ['name', 'description'],
-  // validate: validateSettings,
-  // asyncValidate,
-  // asyncBlurFields: ['username', 'email', 'currentPassword']
-}, mapStateToProps, mapDispatchToProps)(ClassroomOwnerSettings);*/
-
-
-// //////////////////////////////////////////////////////////////
-
-
 import React, { PropTypes } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
@@ -300,20 +159,28 @@ class ClassroomSettingsForm extends React.Component {
         </header>
         <form className="classroom-settings__form">
           Name:
+          <br />
           <input
+            className="classroom-settings__form-element"
             type="text"
             value={this.state.newName}
             onChange={this.handleNameChange}
           />
           <br />
           Description:
-          <input
+          <br />
+          <textarea
+            className="classroom-settings__form-element classroom-settings__form-element-textarea"
             type="text"
             value={this.state.newDescription}
             onChange={this.handleDescriptionChange}
-          />
-          <div>
+            cols="50"
+            rows="4"
+          >
+          </textarea>
+          <div className="classroom-settings__form-element">
             Instructors:
+            <br />
             <ReactTags
               tags={instructorNames}
               handleDelete={this.handleInstructorDelete}
@@ -321,8 +188,9 @@ class ClassroomSettingsForm extends React.Component {
               placeholder={'Add instructor'}
             />
           </div>
-          <div>
+          <div className="classroom-settings__form-element">
             Students:
+            <br />
             <ReactTags
               tags={studentNames}
               handleDelete={this.handleStudentDelete}
@@ -330,7 +198,12 @@ class ClassroomSettingsForm extends React.Component {
               placeholder={'Add student'}
             />
           </div>
-          <input type="button" value="Update" onClick={this.handleUpdateClassroom} />
+          <input
+            className="classroom-settings__form-element"
+            type="button"
+            value="Update"
+            onClick={this.handleUpdateClassroom}
+          />
         </form>
       </section>
     );
