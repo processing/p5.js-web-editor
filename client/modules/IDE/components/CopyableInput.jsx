d121c2c882342b8dd6b26d17e6ddecedfb76313f
Cassie Tarakajian <ctarakajian@gmail.com>
2017-10-30 17:24:43 -0400

    fix and finish #391

--- client/modules/IDE/components/CopyableInput.jsx
+++ client/modules/IDE/components/CopyableInput.jsx
@@ -2,14 +2,29 @@ import React, { PropTypes } from 'react';
 import Clipboard from 'clipboard';
 
 class CopyableInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this);
+  }
+
   componentDidMount() {
     this.clipboard = new Clipboard(this.input, {
       target: () => this.input
+    });
+
+    this.clipboard.on('success', (e) => {
+      this.tooltip.classList.add('tooltipped');
+      this.tooltip.classList.add('tooltipped-n');
     });
   }
 
   componentWillUnmount() {
     this.clipboard.destroy();
+  }
+
+  onMouseLeaveHandler() {
+    this.tooltip.classList.remove('tooltipped');
+    this.tooltip.classList.remove('tooltipped-n');
   }
 
   render() {
@@ -20,19 +35,20 @@ class CopyableInput extends React.Component {
     return (
       <div className="copyable-input">
         <label className="copyable-input__label" htmlFor="copyable-input__value">{label}</label>
-        <div className="copyable-input__value-container">
+        <div
+          className="copyable-input__value-container"
+          aria-label="Copied to Clipboard!"
+          ref={(element) => { this.tooltip = element; }}
+          onMouseLeave={this.onMouseLeaveHandler}
+        >
           <input
             type="text"
             className="copyable-input__value"
             id="copyable-input__value"
             value={value}
             ref={(element) => { this.input = element; }}
+            readOnly
           />
-          <span
-            className="copyable-input__tooltip tooltipped tooltipped-n"
-            aria-label="Copyied to Clipboard!"
-          >
-          </span>
         </div>
       </div>
     );
