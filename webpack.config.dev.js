5738b07515b2c1bfc86aeeeed5f00466c2fb8e6b
Cassie Tarakajian <ctarakajian@gmail.com>
2018-01-09 12:40:44 -0500

    fix bug where undefined environment variables client side were the string 'undefined'

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -34,15 +34,15 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        API_URL: '"' + process.env.API_URL + '"',
+        API_URL: process.env.API_URL ? '"' + process.env.API_URL + '"' : undefined,
         CLIENT: JSON.stringify(true),
         FORCE_TO_HTTPS: process.env.FORCE_TO_HTTPS === 'true' ?
           JSON.stringify(true) :
           JSON.stringify(false),
         'NODE_ENV': JSON.stringify('development'),
-        'S3_BUCKET': '"' + process.env.S3_BUCKET + '"',
-        'S3_BUCKET_URL_BASE': '"' + process.env.S3_BUCKET_URL_BASE + '"',
-        'AWS_REGION': '"' + process.env.AWS_REGION + '"',
+        'S3_BUCKET': process.env.S3_BUCKET ? '"' + process.env.S3_BUCKET + '"' : undefined,
+        'S3_BUCKET_URL_BASE': process.env.S3_BUCKET_URL_BASE ? '"' + process.env.S3_BUCKET_URL_BASE + '"' : undefined,
+        'AWS_REGION': process.env.AWS_REGION ? '"' + process.env.AWS_REGION + '"': undefined
       }
     })
   ],
