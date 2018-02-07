69830b321b3adfc8038ec5c8c4469cd25ef00c5e
joeyklee <j@jk-lee.com>
2018-02-07 16:52:12 -0500

    linted examples-gg-latest

--- server/examples-gg-latest.js
+++ server/examples-gg-latest.js
@@ -70,21 +70,23 @@ mongoose.connection.on('error', () => {
 
 
 /* --- Helper functions --- */
-var flatten = function flatten(list) {
-  return list.reduce(function(a, b) {
-    return a.concat(Array.isArray(b) ? flatten(b) : b);
-  }, []);
+const flatten = function flatten(list) {
+  return list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 };
 
-const insert = function insert(main_string, ins_string, pos) {
-  if (typeof(pos) == "undefined") {
+const insert = function insert(_mainString, _insString, _pos) {
+  let insString = _insString;
+  const mainString = _mainString;
+  let pos = _pos;
+
+  if (typeof (pos) === 'undefined') {
     pos = 0;
   }
-  if (typeof(ins_string) == "undefined") {
-    ins_string = '';
+  if (typeof (insString) === 'undefined') {
+    insString = '';
   }
-  return main_string.slice(0, pos) + ins_string + main_string.slice(pos);
-}
+  return mainString.slice(0, pos) + insString + mainString.slice(pos);
+};
 
 // TEMP: GATHER DATA FROM STATIC FILE
 // - to save time use local json file for now -
@@ -93,10 +95,9 @@ const fs = require('fs');
 // gg-github-newProjects.json
 function retrieveDataTemp(fName) {
   return new Promise((resolve, reject) => {
-    let ggdata = __dirname + "/" + fName;
+    const ggdata = `${__dirname}/${fName}`;
     resolve(JSON.parse(fs.readFileSync(ggdata)));
-
-  })
+  });
 }
 
 /* --- data processing --- */
@@ -120,16 +121,15 @@ function getCodePackage() {
     const json = JSON.parse(res);
 
     json.forEach((metadata) => {
-      if (metadata.name.endsWith("P") === true | metadata.name.endsWith("M") === true) {
+      if (metadata.name.endsWith('P') === true || metadata.name.endsWith('M') === true) {
         sketchRootList.push(metadata);
       }
-    })
+    });
 
     return sketchRootList;
   }).catch((err) => {
     throw err;
   });
-
 }
 
 // 2. get the list of all the top-level sketch directories in P and M
@@ -138,44 +138,40 @@ function getSketchDirectories(sketchRootList) {
 
   return Q.all(sketchRootList.map((sketches) => {
       // console.log(sketches)
-      const options = {
-        url: `https://api.github.com/repos/generative-design/Code-Package-p5.js/contents/${sketches.path}${branchRef}`,
-        qs: {
-          client_id: clientId,
-          client_secret: clientSecret
-        },
-        method: 'GET',
-        headers
-      }
+    const options = {
+      url: `https://api.github.com/repos/generative-design/Code-Package-p5.js/contents/${sketches.path}${branchRef}`,
+      qs: {
+        client_id: clientId,
+        client_secret: clientSecret
+      },
+      method: 'GET',
+      headers
+    };
 
-      return rp(options).then((res) => {
-        let sketchDirs = flatten(JSON.parse(res));
+    return rp(options).then((res) => {
+      const sketchDirs = flatten(JSON.parse(res));
 
-        return sketchDirs;
-      }).catch((err) => {
-        throw err;
-      });
-
-    })
+      return sketchDirs;
+    }).catch((err) => {
+      throw err;
+    });
+  })
 
   ).then((output) => {
     const sketchList = [];
     output.forEach((l) => {
       l.forEach((i) => {
-        if (i.type == "dir")
-          sketchList.push(i);
-      })
-    })
+        if (i.type === 'dir') { sketchList.push(i); }
+      });
+    });
 
     return sketchList;
-  })
-
+  });
 }
 
 
 // 3. For each sketch item in the sketchList, append the tree contents to each item
 function appendSketchItemLinks(sketchList) {
-
   return Q.all(sketchList.map((sketches) => {
     const options = {
       // url: `${sketches.url}?client_id=${clientId}&client_secret=${clientSecret}`,
@@ -186,28 +182,24 @@ function appendSketchItemLinks(sketchList) {
       },
       method: 'GET',
       headers
-    }
+    };
 
     return rp(options).then((res) => {
       const sketchItems = JSON.parse(res);
       sketches.tree = sketchItems;
 
       return sketchList;
-    })
-
-  }))
-
+    });
+  }));
 }
 
-// 4. for each sketch item 
+// 4. for each sketch item
 function getSketchItems(sketchList) {
-
   const completeSketchPkg = [];
 
+  /* eslint-disable */
   return Q.all(sketchList[0].map(sketch => Q.all(sketch.tree.map((item) => {
-    if (item.name == "data") {
-      // console.log(item.path);
-
+    if (item.name === 'data') {
       const options = {
         url: `https://api.github.com/repos/generative-design/Code-Package-p5.js/contents/${item.path}${branchRef}`,
         qs: {
@@ -216,131 +208,126 @@ function getSketchItems(sketchList) {
         },
         method: 'GET',
         headers
-      }
+      };
 
       return rp(options).then((res) => {
         sketch.data = JSON.parse(res);
         return sketch;
       }).catch((err) => {
-        throw err
-      })
-
-    } else {
-      // pass
+        throw err;
+      });
     }
-
-  })))).then(() => {
-    return sketchList[0]
-  })
-
+    // pass
+  })))).then(() => sketchList[0]);
+  /* eslint-enable */
 }
 
 function formatSketchForStorage(sketch, user) {
+  // get the sketch download url
+  function getSketchDownloadUrl(_sketch) {
+    let downloadUrl = '';
+    _sketch.tree.forEach((item) => {
+      if (item.name === 'sketch.js') {
+        downloadUrl += item.download_url;
+      }
+    });
+    return downloadUrl;
+  }
+
   // create a new project template
-  let newProject;
   const a = objectID().toHexString();
   const b = objectID().toHexString();
   const c = objectID().toHexString();
   const r = objectID().toHexString();
 
-  newProject = new Project({
+
+  const newProject = new Project({
     name: sketch.name,
     user: user._id,
     files: [{
-        name: 'root',
-        id: r,
-        _id: r,
-        children: [a, b, c],
-        fileType: 'folder'
-      },
-      {
-        name: 'sketch.js',
-        content: getSketchDownloadUrl(sketch),
-        id: a,
-        _id: a,
-        isSelectedFile: true,
-        fileType: 'file',
-        children: []
-      },
-      {
-        name: 'index.html',
-        content: defaultHTML,
-        id: b,
-        _id: b,
-        fileType: 'file',
-        children: []
-      },
-      {
-        name: 'style.css',
-        content: defaultCSS,
-        id: c,
-        _id: c,
-        fileType: 'file',
-        children: []
-      }
+      name: 'root',
+      id: r,
+      _id: r,
+      children: [a, b, c],
+      fileType: 'folder'
+    },
+    {
+      name: 'sketch.js',
+      content: getSketchDownloadUrl(sketch),
+      id: a,
+      _id: a,
+      isSelectedFile: true,
+      fileType: 'file',
+      children: []
+    },
+    {
+      name: 'index.html',
+      content: defaultHTML,
+      id: b,
+      _id: b,
+      fileType: 'file',
+      children: []
+    },
+    {
+      name: 'style.css',
+      content: defaultCSS,
+      id: c,
+      _id: c,
+      fileType: 'file',
+      children: []
+    }
     ],
     _id: shortid.generate()
 
   });
 
-  // get the sketch download url
-  function getSketchDownloadUrl(sketch) {
-    let downloadUrl = '';
-    sketch.tree.forEach((item) => {
-      if (item.name == "sketch.js") {
-        downloadUrl = downloadUrl + item.download_url;
-      }
-    })
-    return downloadUrl;
-  }
 
   // get any additional js files url
   // TODO: this could probably be optimized - so many loops!
-  function addAdditionalJs(sketch, newProjectObject) {
-    let output = newProjectObject.files;
+  function addAdditionalJs(_sketch, newProjectObject) {
+    const output = newProjectObject.files;
 
-    sketch.tree.forEach((item) => {
-      if (item.name.endsWith(".js") == true && item.name !== "sketch.js") {
+    _sketch.tree.forEach((item) => {
+      if (item.name.endsWith('.js') === true && item.name !== 'sketch.js') {
         const itemId = objectID().toHexString();
 
-        let projectItem = {
+        const projectItem = {
           name: item.name,
           content: item.download_url,
           id: itemId,
           _id: itemId,
           fileType: 'file',
           children: []
-        }
+        };
 
         // push the projectItem to the files array
         output.push(projectItem);
         // add the ID to the root children id array
         output[0].children.push(projectItem.id);
         //  add the JS reference to the defaultHTML
-        output[2].content = insert(output[2].content, `<script src='${item.name}'></script>`, output[2].content.search("<!-- sketch additions -->"));
+        output[2].content = insert(output[2].content, `<script src='${item.name}'></script>`, output[2].content.search('<!-- sketch additions -->'));
       }
-    })
+    });
 
     // if there's output return it, if not pass
     if (output.length > 0) {
       return output;
-    } else {
-      console.log("ERR with addAdditionalJs!")
-      return null
     }
+    console.log('ERR with addAdditionalJs!');
+    return null;
   }
   newProject.files = addAdditionalJs(sketch, newProject);
 
   // fill the data folder with the data files for each sketch
-  function fillDataFolder(sketch, newProjectObject) {
-    const dataInProject = sketch.data || [];
-    let output = newProjectObject.files
+  function fillDataFolder(_sketch, newProjectObject) {
+    const dataInProject = _sketch.data || [];
+    const output = newProjectObject.files;
 
     // create a data folder if there are data in the project
     if (dataInProject.length > 0) {
       const objId = objectID().toHexString();
 
-      let dataFolderIndex = output.length
+      const dataFolderIndex = output.length;
       output.push({
         name: 'data',
         id: objId,
@@ -366,7 +353,7 @@ function formatSketchForStorage(sketch, user) {
         // console.log(`create data: ${item.name}`);
         // add asset file inside the newly created assets folder at last position
         output[dataFolderIndex].children.push(fileID);
-      })
+      });
     }
     return output;
   }
@@ -374,49 +361,45 @@ function formatSketchForStorage(sketch, user) {
 
   // return the newProject
   // console.log(newProject);
-  return newProject
-
+  return newProject;
 }
 
 // format all the sketches using the formatSketchForStorage()
 function formatAllSketches(sketchList) {
   return new Promise((resolve, reject) => {
     User.findOne({ username: 'generative-design' }, (err, user) => {
-      let output = [];
+      const output = [];
       sketchList.forEach((sketch) => {
-        let newProject = formatSketchForStorage(sketch, user);
+        const newProject = formatSketchForStorage(sketch, user);
         output.push(newProject);
-      })
+      });
       resolve(output);
-    })
-
-  })
+    });
+  });
 }
 
 
 // get all the sketch data content and download to the newProjects array
 function getAllSketchContent(newProjectList) {
-
+  /* eslint-disable */
   return Q.all(newProjectList.map(newProject => Q.all(newProject.files.map((sketchFile, i) => {
-
     /*
       sketchFile.name.endsWith(".mp4") !== true &&
       sketchFile.name.endsWith(".ogg") !== true &&
-      sketchFile.name.endsWith(".otf") !== true && 
-      sketchFile.name.endsWith(".ttf") !== true && 
-      sketchFile.name.endsWith(".vvt") !== true && 
-      sketchFile.name.endsWith(".jpg") !== true && 
+      sketchFile.name.endsWith(".otf") !== true &&
+      sketchFile.name.endsWith(".ttf") !== true &&
+      sketchFile.name.endsWith(".vvt") !== true &&
+      sketchFile.name.endsWith(".jpg") !== true &&
       sketchFile.name.endsWith(".png") !== true &&
-      sketchFile.name.endsWith(".svg") !== true 
+      sketchFile.name.endsWith(".svg") !== true
     */
 
-    if (sketchFile.fileType == 'file' &&
+    if (sketchFile.fileType === 'file' &&
       sketchFile.content != null &&
-      sketchFile.name.endsWith(".html") !== true &&
-      sketchFile.name.endsWith(".css") !== true &&
-      sketchFile.name.endsWith(".js") == true
+      sketchFile.name.endsWith('.html') !== true &&
+      sketchFile.name.endsWith('.css') !== true &&
+      sketchFile.name.endsWith('.js') === true
     ) {
-
       const options = {
         url: newProject.files[i].content,
         qs: {
@@ -430,37 +413,29 @@ function getAllSketchContent(newProjectList) {
       // console.log("CONVERT ME!")
       return rp(options).then((res) => {
         newProject.files[i].content = res;
-        return newProject
+        return newProject;
       }).catch((err) => {
-        throw err
-      })
-
-    } else{
-      if(newProject.files[i].url) {
-        return new Promise( (resolve, reject) => {
-          console.log(sketchFile.name);
-          // https://cdn.rawgit.com/opensourcedesign/fonts/2f220059/gnu-freefont_freesans/FreeSans.otf?raw=true
-          // "https://raw.githubusercontent.com/generative-design/Code-Package-p5.js/gg4editor/01_P/P_3_2_1_01/data/FreeSans.otf",
-          const rawGitRef = `https://cdn.rawgit.com/${newProject.files[i].url.split(".com/")[1]}`;
-          sketchFile.content = rawGitRef;
-          sketchFile.url = rawGitRef;
-
-          // replace ref in sketch.js ==> should serve from the file?
-          // newProject.files[1].content = newProject.files[1].content.replace(`'data/${sketchFile.name}'`, `'${rawGitRef}'`);      
-          resolve(newProject);
-
-        })
-        
-      }
+        throw err;
+      });
     }
+    if (newProject.files[i].url) {
+      return new Promise((resolve, reject) => {
+        console.log(sketchFile.name);
+        // https://cdn.rawgit.com/opensourcedesign/fonts/2f220059/gnu-freefont_freesans/FreeSans.otf?raw=true
+        // "https://raw.githubusercontent.com/generative-design/Code-Package-p5.js/gg4editor/01_P/P_3_2_1_01/data/FreeSans.otf",
+        const rawGitRef = `https://cdn.rawgit.com/${newProject.files[i].url.split('.com/')[1]}`;
+        sketchFile.content = rawGitRef;
+        sketchFile.url = rawGitRef;
 
+        // replace ref in sketch.js ==> should serve from the file?
+        // newProject.files[1].content = newProject.files[1].content.replace(`'data/${sketchFile.name}'`, `'${rawGitRef}'`);
+        resolve(newProject);
+      });
+    }
   })).catch((err) => {
-    throw err
-  }))).then(() => {
-
-    return newProjectList;
-  })
-
+    throw err;
+  }))).then(() => newProjectList);
+  /* eslint-enable */
 }
 
 // Save the project in the db
@@ -469,13 +444,12 @@ function createProjectsInP5user(newProjectList) {
     if (err) throw err;
 
     eachSeries(newProjectList, (newProject, sketchCallback) => {
-
       newProject.save((saveErr, savedProject) => {
         if (saveErr) throw saveErr;
         console.log(`Created a new project in p5 user: ${savedProject.name}`);
         sketchCallback();
       });
-    })
+    });
   }).catch((err) => {
     throw err;
   });
@@ -485,7 +459,6 @@ function createProjectsInP5user(newProjectList) {
 /* --- Main --- */
 // remove any of the old files and add the new stuffs to the UI
 function getp5User() {
-
   User.findOne({ username: 'generative-design' }, (err, user) => {
     if (err) throw err;
 
@@ -510,51 +483,41 @@ function getp5User() {
           if (removeErr) throw removeErr;
         });
       });
-
-
     });
 
 
-    if(testMake == true){
+    if (testMake === true) {
       // Run for Testing
-      // return retrieveDataTemp()
-      //   .then(formatAllSketches)
-      //   .then(getAllSketchContent)
-      //   .then(saveToFile)
-      //   .then(createProjectsInP5user)
-
-      // Run for Testing
-      // return retrieveDataTemp('gg-github-retrieval.json')
-      //   .then(formatAllSketches)
-      //   .then(getAllSketchContent)
-      //   // .then(saveToFile)
-      //   .then(createProjectsInP5user)
-
-    } else {
+      // Run for production
+      return getCodePackage()
+        .then(getSketchDirectories)
+        .then(appendSketchItemLinks)
+        .then(getSketchItems)
+        // .then(saveRetrievalToFile)
+        .then(formatAllSketches)
+        .then(getAllSketchContent)
+        // .then(saveNewProjectsToFile)
+        .then(createProjectsInP5user);
+    }
       // Run for production
     return getCodePackage()
-      .then(getSketchDirectories)
-      .then(appendSketchItemLinks)
-      .then(getSketchItems)
-      .then(saveRetrievalToFile)
-      .then(formatAllSketches)
-      .then(getAllSketchContent)
-      .then(saveNewProjectsToFile)
-      .then(createProjectsInP5user)
-    }
-
-  })
-
+        .then(getSketchDirectories)
+        .then(appendSketchItemLinks)
+        .then(getSketchItems)
+        .then(formatAllSketches)
+        .then(getAllSketchContent)
+        .then(createProjectsInP5user);
+  });
 }
 // Run the entire process
 getp5User();
 
 
 /* --- Tester Functions --- */
-/*** Tester Functions - IGNORE BELOW
+/** * Tester Functions - IGNORE BELOW
 @ below are functions for testing
 output etc
-***/
+** */
 // formatSketchForStorage(P_2_1_1_04);
 // formatSketchForStorage(M_1_5_04);
 
@@ -569,9 +532,9 @@ output etc
 //         const rawGitRef = `https://cdn.rawgit.com/${newProject.files[i].url.split(".com/")[1]}`;
 //         sketchFile.content = rawGitRef;
 //         newProject.files[1].content = newProject.files[1].content.replace(`'data/${sketchFile.name}'`, `'${rawGitRef}'`);
-        
+
 //         resolve(newProject)
-//       } 
+//       }
 //     })
 
 //   })).catch((err) => {
@@ -582,33 +545,33 @@ output etc
 //   })
 // }
 
-// checking function 
+// checking function
 function doNext(output) {
-  console.log( JSON.stringify(output));
+  console.log(JSON.stringify(output));
   console.log(output.length);
 }
 
 // save output to terminal
 function saveRetrievalToFile(output) {
-  return new Promise( (resolve, reject) => {
-    fs.writeFileSync(`server/gg-github-raw.json`, JSON.stringify(output));
-    resolve( output )
-  }) 
+  return new Promise((resolve, reject) => {
+    fs.writeFileSync('server/gg-github-raw.json', JSON.stringify(output));
+    resolve(output);
+  });
 }
 
 // save output to terminal
 function saveNewProjectsToFile(output) {
-  return new Promise( (resolve, reject) => {
-    fs.writeFileSync(`server/gg-github-newProjects.json`, JSON.stringify(output));
-    resolve( output )
-  }) 
+  return new Promise((resolve, reject) => {
+    fs.writeFileSync('server/gg-github-newProjects.json', JSON.stringify(output));
+    resolve(output);
+  });
 }
 // test make without deleting all projects etc
-function make() {
-  return retrieveDataTemp('gg-github-retrieval.json')
-    .then(formatAllSketches)
-    .then(getAllSketchContent)
-    .then(linkToFontFiles)
-    .then(saveToFile)
-}
+// function make() {
+//   return retrieveDataTemp('gg-github-retrieval.json')
+//     .then(formatAllSketches)
+//     .then(getAllSketchContent)
+//     .then(linkToFontFiles)
+//     .then(saveToFile);
+// }
 // make();
