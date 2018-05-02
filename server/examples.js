bc36a79b94f4202e3cec45fd838c3d310eaf584c
Bharathvaj <bharathvaj1995@users.noreply.github.com>
2018-05-03 00:45:33 +0530

    Move examples username and password to .env file (#556)
    * Move examples username and password to .env file
    
    * Update README.md
    
    * Update examples.js
    
    * Update examples.js

--- server/examples.js
+++ server/examples.js
@@ -135,7 +135,9 @@ function createProjectsInP5user(projectsInAllCategories) {
   };
 
   rp(options).then((res) => {
-    User.findOne({ username: 'p5' }, (err, user) => {
+    const assets = JSON.parse(res);
+
+    User.findOne({ username: process.env.EXAMPLE_USERNAME }, (err, user) => {
       if (err) throw err;
 
       eachSeries(projectsInAllCategories, (projectsInOneCategory, categoryCallback) => {
@@ -287,15 +289,15 @@ function createProjectsInP5user(projectsInAllCategories) {
 }
 
 function getp5User() {
-  User.findOne({ username: 'p5' }, (err, user) => {
+  User.findOne({ username: process.env.EXAMPLE_USERNAME }, (err, user) => {
     if (err) throw err;
 
     let p5User = user;
     if (!p5User) {
       p5User = new User({
-        username: 'p5',
-        email: 'p5-examples@gmail.com',
-        password: 'test'
+        username: process.env.EXAMPLE_USERNAME,
+	email: process.env.EXAMPLE_USER_EMAIL,
+	password: process.env.EXAMPLE_USER_PASSWORD
       });
       p5User.save((saveErr) => {
         if (saveErr) throw saveErr;
