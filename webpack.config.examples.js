a14c232935153e7b358979946c9092c2f94007e9
Cassie Tarakajian <ctarakajian@gmail.com>
2018-06-13 17:40:46 -0700

    fix fetch example scripts, add webpack config to build fetch script bundles

---
+++ webpack.config.examples.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = [{
+  entry: path.resolve(__dirname, 'server/scripts/fetch-examples.js'),
+
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'fetch-examples.bundle.js'
+  },
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    modules: [
+      'client',
+      'node_modules',
+    ],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'react',
+            'env',
+            'stage-0',
+          ],
+          plugins: [
+            [
+              'babel-plugin-webpack-loaders', {
+                'config': './webpack.config.babel.js',
+                "verbose": false
+              }
+            ]
+          ]
+        },
+      }
+    ],
+  },
+},
+{
+  entry: path.resolve(__dirname, 'server/scripts/fetch-examples-gg.js'),
+
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'fetch-examples-gg.bundle.js'
+  },
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    modules: [
+      'client',
+      'node_modules',
+    ],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'react',
+            'env',
+            'stage-0',
+          ],
+          plugins: [
+            [
+              'babel-plugin-webpack-loaders', {
+                'config': './webpack.config.babel.js',
+                "verbose": false
+              }
+            ]
+          ]
+        },
+      }
+    ],
+  },
+}];
