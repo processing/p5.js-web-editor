f6f8e145d1fc92118b874cbe0f4c3aeb29b33dc5
Cassie Tarakajian <ctarakajian@gmail.com>
2018-06-25 19:16:42 -0700

    get the mongo authentication to workgit add .

--- mongo/init.sh
+++ mongo/init.sh
@@ -1,1 +1,1 @@
-mongo $MONGO_INITDB_DATABASE --eval "db.createUser({ user: '$MONGO_RW_USERNAME', pwd: '$MONGO_RW_PASSWORD', roles: [ 'readWrite' ] })"
+mongo admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.createUser({ user: '$MONGO_RW_USERNAME', pwd: '$MONGO_RW_PASSWORD', roles: [ { role: 'readWrite', db: '$MONGO_INITDB_DATABASE' },  { role: 'readWrite', db: 'sessions' }] })"
