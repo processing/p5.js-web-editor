4dc0ee1d25473532179c0896e7efbd988b96c8c7
Cassie Tarakajian <ctarakajian@gmail.com>
2018-08-21 17:52:42 -0400

    fix linting errors

--- server/server.js
+++ server/server.js
@@ -33,9 +33,7 @@ import { get404Sketch } from './views/404Page';
 const app = new Express();
 const MongoStore = connectMongo(session);
 
-app.get('/health', (req, res) => {
-  return res.json({success: true});
-});
+app.get('/health', (req, res) => res.json({ success: true }));
 
 // For basic auth, in setting up beta editor
 if (process.env.BASIC_USERNAME && process.env.BASIC_PASSWORD) {
