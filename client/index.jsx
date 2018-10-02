aba14fc356557bd03986bb21c6e3abb2f5355ce3
Vertmo <basile.pesin@gmail.com>
2018-10-02 22:51:32 +0200

    Corrected the logo problem by adding it to the static files

--- client/index.jsx
+++ client/index.jsx
@@ -8,6 +8,9 @@ import routes from './routes';
 
 require('./styles/main.scss');
 
+// Load the p5 png logo, so that webpack will use it
+require('./images/p5js-square-logo.png');
+
 const history = browserHistory;
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState);
