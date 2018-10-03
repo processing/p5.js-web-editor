cb5da4720595189aeb6760cc50282ebf300778fa
Vertmo <basile.pesin@gmail.com>
2018-10-03 23:03:51 +0200

    Got the basis covered, now I need to style all this

--- server/routes/embed.routes.js
+++ server/routes/embed.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import * as FullController from '../controllers/full.controller';
 import * as EmbedController from '../controllers/embed.controller';
 
 const router = new Router();
 
-router.get('/embed/:project_id', EmbedController.serveProject);
-router.get('/full/:project_id', EmbedController.serveProject);
+router.get('/:username/full/:project_id', FullController.serveProject);
+
+router.get('/:username/embed/:project_id', EmbedController.serveProject);
+router.get('/:username/presentation/:project_id', EmbedController.serveProject);
+router.get('/:username/pres/:project_id', EmbedController.serveProject);
 
 export default router;
