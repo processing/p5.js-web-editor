c3f2a537fcedc840a44b40bf78e976820b90ee28
shinytang6 <1074461480@qq.com>
2018-10-04 21:06:55 +0800

    update result/command icon

--- client/modules/IDE/components/Console.jsx
+++ client/modules/IDE/components/Console.jsx
@@ -38,13 +38,17 @@ class Console extends React.Component {
       LOG_WARN_ICON: `url(${warnLightUrl})`,
       LOG_ERROR_ICON: `url(${errorLightUrl})`,
       LOG_DEBUG_ICON: `url(${debugLightUrl})`,
-      LOG_INFO_ICON: `url(${infoLightUrl})`
+      LOG_INFO_ICON: `url(${infoLightUrl})`,
+      LOG_COMMAND_ICON: `url(${leftArrowUrl})`,
+      LOG_RESULT_ICON: `url(${rightArrowUrl})`
     };
     const CONSOLE_FEED_DARK_ICONS = {
       LOG_WARN_ICON: `url(${warnDarkUrl})`,
       LOG_ERROR_ICON: `url(${errorDarkUrl})`,
       LOG_DEBUG_ICON: `url(${debugDarkUrl})`,
-      LOG_INFO_ICON: `url(${infoDarkUrl})`
+      LOG_INFO_ICON: `url(${infoDarkUrl})`,
+      LOG_COMMAND_ICON: `url(${leftArrowUrl})`,
+      LOG_RESULT_ICON: `url(${rightArrowUrl})`
     };
     const CONSOLE_FEED_SIZES = {
       TREENODE_LINE_HEIGHT: 1.2,
