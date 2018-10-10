2307d89df77526fb8f0baa59221ca2e18256688c
Cassie Tarakajian <ctarakajian@gmail.com>
2018-10-10 18:29:53 -0400

    for #727

--- server/routes/asset.routes.js
+++ server/routes/asset.routes.js
@@ -5,6 +5,7 @@ import { getFileContent } from '../controllers/file.controller';
 const router = new Router();
 
 router.get('/:username/sketches/:project_id/*', getProjectAsset);
+router.get('/full/:project_id/*', getProjectAsset);
 
 router.get('/sketches/:project_id/assets/*?', getFileContent);
 
