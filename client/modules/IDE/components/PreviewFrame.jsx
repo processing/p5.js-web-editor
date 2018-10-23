processing/p5.js-web-editor

commit 8b2133ea998c106eb1edf95fb27f1b7d649e215e

fixes #740

client/modules/IDE/components/PreviewFrame.jsx
@@ -241,7 +241,8 @@ class PreviewFrame extends React.Component {
             // could also pull file from API instead of using bloburl
             const blobURL = getBlobUrl(resolvedFile);
             this.props.setBlobUrl(resolvedFile, blobURL);
-            newContent = newContent.replace(filePath, blobURL);
+            const filePathRegex = new RegExp(filePath, "gi");
+            newContent = newContent.replace(filePathRegex, blobURL);
           }
         }
       }

commit 5196a3780793784c1d8f8e27cfc0c0f230289ee1

fix linting error caused by 8b2133e

client/modules/IDE/components/PreviewFrame.jsx
@@ -241,7 +241,7 @@ class PreviewFrame extends React.Component {
             // could also pull file from API instead of using bloburl
             const blobURL = getBlobUrl(resolvedFile);
             this.props.setBlobUrl(resolvedFile, blobURL);
-            const filePathRegex = new RegExp(filePath, "gi");
+            const filePathRegex = new RegExp(filePath, 'gi');
             newContent = newContent.replace(filePathRegex, blobURL);
           }
         }