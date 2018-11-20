402fb8ff55d2ec2230c5aed7cc753bf1a9417c57
Vertmo <basile.pesin@gmail.com>
2018-11-20 19:52:11 +0100

    Fixed crash when using console.error()

--- client/modules/IDE/components/Editor.jsx
+++ client/modules/IDE/components/Editor.jsx
@@ -194,11 +194,11 @@ class Editor extends React.Component {
     if (this.props.runtimeErrorWarningVisible && this._cm.getDoc().modeOption === 'javascript') {
       this.props.consoleEvents.forEach((consoleEvent) => {
         if (consoleEvent.method === 'error') {
-          if (consoleEvent.arguments.indexOf(')') > -1) {
-            const n = consoleEvent.arguments.replace(')', '').split(' ');
-            const lineNumber = parseInt(n[n.length - 1], 10) - 1;
-            this._cm.addLineClass(lineNumber, 'background', 'line-runtime-error');
-          }
+        //   if (consoleEvent.arguments.indexOf(')') > -1) {
+        //     const n = consoleEvent.arguments.replace(')', '').split(' ');
+        //     const lineNumber = parseInt(n[n.length - 1], 10) - 1;
+        //     this._cm.addLineClass(lineNumber, 'background', 'line-runtime-error');
+        //   }
         }
       });
     }
