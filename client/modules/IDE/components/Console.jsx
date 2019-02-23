f41d70ee6870837e883cd4190e69286535d52855
Peter <pwjablonski@gmail.com>
2019-02-23 15:42:32 -0500

    Add more padding and update log_icon_height #711

--- client/modules/IDE/components/Console.jsx
+++ client/modules/IDE/components/Console.jsx
@@ -48,12 +48,12 @@ class Console extends React.Component {
       LOG_INFO_ICON: `url(${infoDarkUrl})`
     };
     const CONSOLE_FEED_SIZES = {
-      PADDING: '4px 22px 2px 0',
+      PADDING: '6px 22px 2px 0',
       TREENODE_LINE_HEIGHT: 1.2,
       BASE_FONT_SIZE: this.props.fontSize,
       ARROW_FONT_SIZE: this.props.fontSize,
       LOG_ICON_WIDTH: this.props.fontSize,
-      LOG_ICON_HEIGHT: 1.45 * this.props.fontSize,
+      LOG_ICON_HEIGHT: this.props.fontSize,
     };
 
     if (times > 1) {
