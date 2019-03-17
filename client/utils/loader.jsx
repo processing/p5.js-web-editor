68fc75e4bd9131a60bc315755c11d19c9752d058
siddhant1 <sidhant.manchanda@gmail.com>
2019-03-17 22:31:29 +0530

    Changed styles

--- client/utils/loader.jsx
+++ client/utils/loader.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Loader = () => (
-  <div className="loader"></div>
+  <div className="loader">
+    <div className="loader__circle1" />
+    <div className="loader__circle2" />
+  </div>
 );
 export default Loader;
