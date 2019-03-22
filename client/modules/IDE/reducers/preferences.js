d1ce054abf67fea0012ed1c725dcf8d022f4148f
cdr <qualitymanifest@gmail.com>
2019-03-21 21:08:59 -0400

    Remove indentation config from actions, reducers, and constants

--- client/modules/IDE/reducers/preferences.js
+++ client/modules/IDE/reducers/preferences.js
@@ -2,8 +2,6 @@ import * as ActionTypes from '../../../constants';
 
 const initialState = {
   fontSize: 18,
-  indentationAmount: 2,
-  isTabIndent: true,
   autosave: true,
   lintWarning: false,
   textOutput: false,
@@ -17,16 +15,6 @@ const preferences = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.SET_FONT_SIZE:
       return Object.assign({}, state, { fontSize: action.value });
-    case ActionTypes.SET_INDENTATION:
-      return Object.assign({}, state, { indentationAmount: action.value });
-    case ActionTypes.INDENT_WITH_TAB:
-      return Object.assign({}, state, {
-        isTabIndent: true
-      });
-    case ActionTypes.INDENT_WITH_SPACE:
-      return Object.assign({}, state, {
-        isTabIndent: false
-      });
     case ActionTypes.SET_AUTOSAVE:
       return Object.assign({}, state, { autosave: action.value });
     case ActionTypes.SET_LINT_WARNING:
