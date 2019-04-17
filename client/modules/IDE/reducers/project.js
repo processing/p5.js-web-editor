7d1901649f9849c294cba06c29109dc9a1ced05f
Cassie Tarakajian <ctarakajian@gmail.com>
2019-04-17 14:08:33 -0400

    Project synching, for #790 (#1039)
    * add isSaving to project reducer, move actions to functions, start work to get comprehensive frontend/backend syncing working
    
    * handle making changes while saving project, handle saving from another window
    
    * add change to handle saving new sketch, and adding new changes while saving

--- client/modules/IDE/reducers/project.js
+++ client/modules/IDE/reducers/project.js
@@ -12,7 +12,8 @@ const initialState = () => {
   const generatedName = generatedString.charAt(0).toUpperCase() + generatedString.slice(1);
   return {
     name: generatedName,
-    updatedAt: ''
+    updatedAt: '',
+    isSaving: false
   };
 };
 
@@ -28,14 +29,16 @@ const project = (state, action) => {
         id: action.project.id,
         name: action.project.name,
         updatedAt: action.project.updatedAt,
-        owner: action.owner
+        owner: action.owner,
+        isSaving: false
       };
     case ActionTypes.SET_PROJECT:
       return {
         id: action.project.id,
         name: action.project.name,
         updatedAt: action.project.updatedAt,
-        owner: action.owner
+        owner: action.owner,
+        isSaving: false
       };
     case ActionTypes.RESET_PROJECT:
       return initialState();
@@ -45,6 +48,10 @@ const project = (state, action) => {
       return Object.assign({}, state, { isEditingName: false });
     case ActionTypes.SET_PROJECT_SAVED_TIME:
       return Object.assign({}, state, { updatedAt: action.value });
+    case ActionTypes.START_SAVING_PROJECT:
+      return Object.assign({}, state, { isSaving: true });
+    case ActionTypes.START_STOP_PROJECT:
+      return Object.assign({}, state, { isSaving: false });
     default:
       return state;
   }
