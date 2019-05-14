824715c434d7b48632593bd42ea00c3436bacfee
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-05-14 12:26:25 +0200

    Do not return any keys in API

--- server/controllers/session.controller.js
+++ server/controllers/session.controller.js
@@ -13,7 +13,7 @@ export function createSession(req, res, next) {
         email: req.user.email,
         username: req.user.username,
         preferences: req.user.preferences,
-        apiKeys: req.user.apiKeys,
+        apiKeys: req.user.publicApiKeys,
         verified: req.user.verified,
         id: req.user._id
       });
@@ -27,7 +27,7 @@ export function getSession(req, res) {
       email: req.user.email,
       username: req.user.username,
       preferences: req.user.preferences,
-      apiKeys: req.user.apiKeys,
+      apiKeys: req.user.publicApiKeys,
       verified: req.user.verified,
       id: req.user._id
     });
