29a59b22785fb29ff5abb21fb474fa71c5be494e
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-05-15 12:11:58 +0200

    Styles Account and APIKeys components

--- client/modules/User/components/APIKeyForm.jsx
+++ client/modules/User/components/APIKeyForm.jsx
@@ -1,67 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import InlineSVG from 'react-inlinesvg';
-import format from 'date-fns/format';
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
-import orderBy from 'lodash/orderBy';
 
-const trashCan = require('../../../images/trash-can.svg');
+import CopyableInput from '../../IDE/components/CopyableInput';
 
+import APIKeyList from './APIKeyList';
 
-function NewTokenDisplay({ token }) {
-  function handleCopyClick() {
-    navigator.clipboard.writeText(token);
-  }
+const plusIcon = require('../../../images/plus-icon.svg');
 
-  return (
-    <React.Fragment>
-      <p>Make sure to copy your new personal access token now. You won’t be able to see it again!</p>
-      <p><input type="text" readOnly value={token} /></p>
-      <button onClick={handleCopyClick}>Copy</button>
-    </React.Fragment>
-  );
-}
-
-function TokenMetadataList({ tokens, onRemove }) {
-  return (
-    <table className="form__table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Created on</th>
-          <th>Last used</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orderBy(tokens, ['createdAt'], ['desc']).map((v) => {
-          const keyRow = (
-            <tr key={v.id}>
-              <td>{v.label}</td>
-              <td>{format(new Date(v.createdAt), 'MMM D, YYYY h:mm A')}</td>
-              <td>{distanceInWordsToNow(new Date(v.lastUsedAt), { addSuffix: true })}</td>
-              <td>
-                <button className="account__tokens__delete-button" onClick={() => onRemove(v)}>
-                  <InlineSVG src={trashCan} alt="Delete Key" />
-                </button>
-              </td>
-            </tr>
-          );
-
-          const newKeyValue = v.token && (
-            <tr key={`${v.id}-newKey`}>
-              <td colSpan="4">
-                <NewTokenDisplay token={v.token} />
-              </td>
-            </tr>
-          );
-
-          return [keyRow, newKeyValue];
-        })}
-      </tbody>
-    </table>
-  );
-}
+export const APIKeyPropType = PropTypes.shape({
+  id: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  createdAt: PropTypes.object.isRequired,
+  lastUsedAt: PropTypes.object.isRequired,
+});
 
 class APIKeyForm extends React.Component {
   constructor(props) {
@@ -99,45 +51,64 @@ class APIKeyForm extends React.Component {
 
     if (hasApiKeys) {
       return (
-        <TokenMetadataList tokens={this.props.apiKeys} onRemove={this.removeKey} />
+        <APIKeyList apiKeys={this.props.apiKeys} onRemove={this.removeKey} />
       );
     }
-    return <p>You have no API keys</p>;
+    return <p>You have no exsiting tokens.</p>;
   }
 
   render() {
+    const keyWithToken = this.props.apiKeys.find(k => !!k.token);
+
     return (
-      <div>
-        <form id="addKeyForm" className="form" onSubmit={this.addKey}>
-          <label htmlFor="keyLabel" className="form__label">Name</label>
-          <input
-            type="text"
-            className="form__input"
-            placeholder="What is this token for?"
-            id="keyLabel"
-            onChange={(event) => { this.setState({ keyLabel: event.target.value }); }}
-          />
-          <input
-            type="submit"
-            value="Create new Key"
-            disabled={this.state.keyLabel === ''}
-          />
-        </form>
-        {this.renderApiKeys()}
+      <div className="api-key-form">
+        <p className="api-key-form__summary">Personal Access Tokens act like your password to allow automated scripts to access the Editor API. Create a token for each script that needs access.</p>
+
+        <div className="api-key-form__section">
+          <h3 className="api-key-form__title">Create new token</h3>
+          <form className="form form--inline" onSubmit={this.addKey}>
+            <label htmlFor="keyLabel" className="form__label form__label--hidden ">What is this token for?</label>
+            <input
+              className="form__input"
+              id="keyLabel"
+              onChange={(event) => { this.setState({ keyLabel: event.target.value }); }}
+              placeholder="What is this token for? e.g. Example import script"
+              type="text"
+              value={this.state.keyLabel}
+            />
+            <button
+              className="api-key-form__create-button"
+              disabled={this.state.keyLabel === ''}
+              type="submit"
+            >
+              <InlineSVG src={plusIcon} alt="" /> Create
+            </button>
+          </form>
+
+          {
+            keyWithToken && (
+              <div className="api-key-form__new-token">
+                <h4 className="api-key-form__new-token__title">Your new access token</h4>
+                <p className="api-key-form__new-token__info">Make sure to copy your new personal access token now. You won’t be able to see it again!</p>
+                <CopyableInput label={keyWithToken.label} value={keyWithToken.token} />
+              </div>
+            )
+          }
+        </div>
+
+        <div className="api-key-form__section">
+          <h3 className="api-key-form__title">Existing tokens</h3>
+          {this.renderApiKeys()}
+        </div>
       </div>
     );
   }
 }
 
 APIKeyForm.propTypes = {
+  apiKeys: PropTypes.arrayOf(PropTypes.shape(APIKeyPropType)).isRequired,
   createApiKey: PropTypes.func.isRequired,
   removeApiKey: PropTypes.func.isRequired,
-  apiKeys: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.object.isRequired,
-    label: PropTypes.string.isRequired,
-    createdAt: PropTypes.object.isRequired,
-    lastUsedAt: PropTypes.object.isRequired,
-  })).isRequired
 };
 
 export default APIKeyForm;
