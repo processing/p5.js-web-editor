0e9bbdc4d8927644046ba3dfeec61c54824c52c5
Cassie Tarakajian <ctarakajian@gmail.com>
2019-06-19 16:56:55 -0400

    fixes #498

--- server/controllers/aws.controller.js
+++ server/controllers/aws.controller.js
@@ -126,21 +126,29 @@ export function listObjectsInS3ForUser(req, res) {
       .on('end', () => {
         const projectAssets = [];
         getProjectsForUserId(userId).then((projects) => {
-          projects.forEach((project) => {
-            project.files.forEach((file) => {
-              if (!file.url) return;
-
-              const foundAsset = assets.find(asset => file.url.includes(asset.key));
-              if (!foundAsset) return;
-              projectAssets.push({
-                name: file.name,
-                sketchName: project.name,
-                sketchId: project.id,
-                url: file.url,
-                key: foundAsset.key,
-                size: foundAsset.size
+          assets.forEach((asset) => {
+            let name = asset.key.split('/').pop();
+            let foundAsset = {
+              key: asset.key,
+              name: name,
+              size: asset.size,
+              url: `${process.env.S3_BUCKET_URL_BASE}${asset.key}`
+            };
+            projects.some((project) => {
+              let found = false;
+              project.files.some((file) => {
+                if (!file.url) return false;
+                if (file.url.includes(asset.key)) {
+                  found = true;
+                  foundAsset.name = file.name;
+                  foundAsset.sketchName = project.name;
+                  foundAsset.sketchId = project.id;
+                  foundAsset.url = file.url;
+                }
               });
+              return found;
             });
+            projectAssets.push(foundAsset);
           });
           res.json({ assets: projectAssets });
         });
