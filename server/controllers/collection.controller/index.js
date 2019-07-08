83978acc1de55c3320bc7e77c0a82785998cc71a
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-07-08 12:06:13 +0200

    Adds Collections model and Editor API to manage collections
    - List any user's collections
    - Create new collection
    - Modify collection metadata
    - Delete collection
    - Add/remove any project to/from a collection

---
+++ server/controllers/collection.controller/index.js
@@ -0,0 +1,6 @@
+export { default as addProjectToCollection } from './addProjectToCollection';
+export { default as createCollection } from './createCollection';
+export { default as listCollections } from './listCollections';
+export { default as removeCollection } from './removeCollection';
+export { default as removeProjectFromCollection } from './removeProjectFromCollection';
+export { default as updateCollection } from './updateCollection';
