d44a058fd861b4f92ac5362e056be81ffd58405d
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-08-30 20:26:57 +0200

    Public API: Create new project (fixes #1095) (#1106)
    * Converts import script to use public API endpoints
    
    The endpoints don't exist yet, but this is a good way to see how
    the implementation of the data structures differ.
    
    * Exposes public API endpoint to fetch user's sketches
    
    * Implements public API delete endpoint
    
    * Adds helper to create custom ApplicationError classes
    
    * Adds create project endpoint that understand API's data structure
    
    This transforms the nested tree of file data into a mongoose
    Project model
    
    * Returns '201 Created' to match API spec
    
    * Removes 'CustomError' variable assignment as it shows up in test output
    
    * transformFiles will return file validation errors
    
    * Tests API project controller
    
    * Tests toModel()
    
    * Creates default files if no root-level .html file is provided
    
    * Do not auto-generate a slug if it is provided
    
    Fixes a bug where the slug was auto-generated using the sketch name,
    even if a slug property had been provided.
    
    * Validates uniqueness of slugs for projects created by the public API
    
    * Adds tests for slug uniqueness
    
    * Configures node's Promise implementation for mongoose (fixes warnings)
    
    * Moves createProject tests to match controller location
    
    * Adds support for code to ApplicationErrors
    
    * deleteProject controller tests
    
    * getProjectsForUser controller tests
    
    - implements tests
    - update apiKey tests to use new User mocks
    
    * Ensure error objects have consistent property names
    
    `message` is used as a high-level description of the errors
    `detail` is optional and has an plain language explanation of the
    individual errors
    `errors` is an array of each individual problem from `detail` in a
    machine-readable format
    
    * Assert environment variables are provided at script start
    
    * Version public API
    
    * Expect "files" property to always be provided
    
    * Fixes linting error
    
    * Converts import script to use public API endpoints
    
    The endpoints don't exist yet, but this is a good way to see how
    the implementation of the data structures differ.
    
    * Exposes public API endpoint to fetch user's sketches
    
    * Implements public API delete endpoint
    
    * Adds helper to create custom ApplicationError classes
    
    * Adds create project endpoint that understand API's data structure
    
    This transforms the nested tree of file data into a mongoose
    Project model
    
    * Returns '201 Created' to match API spec
    
    * Removes 'CustomError' variable assignment as it shows up in test output
    
    * transformFiles will return file validation errors
    
    * Tests API project controller
    
    * Tests toModel()
    
    * Creates default files if no root-level .html file is provided
    
    * Do not auto-generate a slug if it is provided
    
    Fixes a bug where the slug was auto-generated using the sketch name,
    even if a slug property had been provided.
    
    * Validates uniqueness of slugs for projects created by the public API
    
    * Adds tests for slug uniqueness
    
    * Configures node's Promise implementation for mongoose (fixes warnings)
    
    * Moves createProject tests to match controller location
    
    * deleteProject controller tests
    
    * Adds support for code to ApplicationErrors
    
    * getProjectsForUser controller tests
    
    - implements tests
    - update apiKey tests to use new User mocks
    
    * Ensure error objects have consistent property names
    
    `message` is used as a high-level description of the errors
    `detail` is optional and has an plain language explanation of the
    individual errors
    `errors` is an array of each individual problem from `detail` in a
    machine-readable format
    
    * Assert environment variables are provided at script start
    
    * Version public API
    
    * Expect "files" property to always be provided
    
    * Fixes linting error
    
    * Checks that authenticated user has permission to create under this namespace
    
    Previously, the project was always created under the authenticated user's
    namespace, but this not obvious behaviour.

--- server/models/__mocks__/user.js
+++ server/models/__mocks__/user.js
@@ -1,12 +1,31 @@
-let __err = null;
-let __user = null;
+import sinon from 'sinon';
+import 'sinon-mongoose';
 
-export default {
-  __setFindById(err, user) {
-    __err = err;
-    __user = user;
-  },
-  findById: jest.fn(async (id, callback) => {
-    callback(__err, __user);
-  })
-};
+// Import the actual model to be mocked
+const User = jest.requireActual('../user').default;
+
+// Wrap User in a sinon mock
+// The returned object is used to configure
+// the mocked model's behaviour
+export function createMock() {
+  return sinon.mock(User);
+}
+
+// Wraps the User.prototype i.e. the
+// instance methods in a mock so
+// User.save() can be mocked
+export function createInstanceMock() {
+  // See: https://stackoverflow.com/questions/40962960/sinon-mock-of-mongoose-save-method-for-all-future-instances-of-a-model-with-pro
+  Object.defineProperty(User.prototype, 'save', {
+    value: User.prototype.save,
+    configurable: true,
+  });
+
+  return sinon.mock(User.prototype);
+}
+
+
+// Re-export the model, it will be
+// altered by mockingoose whenever
+// we call methods on the MockConfig
+export default User;
---
+++ server/routes/api.routes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import passport from 'passport';
+import * as ProjectController from '../controllers/project.controller';
+
+const router = new Router();
+
+router.get(
+  '/:username/sketches',
+  passport.authenticate('basic', { session: false }),
+  ProjectController.apiGetProjectsForUser
+);
+
+router.post(
+  '/:username/sketches',
+  passport.authenticate('basic', { session: false }),
+  ProjectController.apiCreateProject
+);
+
+// NOTE: Currently :username will not be checked for ownership
+//       only the project's owner in the database.
+router.delete(
+  '/:username/sketches/:project_id',
+  passport.authenticate('basic', { session: false }),
+  ProjectController.deleteProject
+);
+
+export default router;
