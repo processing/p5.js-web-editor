986e19c1ba26cbf6f0892d7e65923e0776be20fa
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2020-02-25 23:21:58 +0100

    Adds theme

--- client/index.jsx
+++ client/index.jsx
@@ -3,8 +3,11 @@ import { render } from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { ThemeProvider } from 'styled-components';
+
 import configureStore from './store';
 import routes from './routes';
+import theme, { Theme } from './theme';
 
 require('./styles/main.scss');
 
@@ -16,10 +19,14 @@ const initialState = window.__INITIAL_STATE__;
 
 const store = configureStore(initialState);
 
+const currentTheme = Theme.light;
+
 const App = () => (
-  <Provider store={store}>
-    <Router history={history} routes={routes(store)} />
-  </Provider>
+  <ThemeProvider theme={{ ...theme, current: theme[currentTheme] }}>
+    <Provider store={store}>
+      <Router history={history} routes={routes(store)} />
+    </Provider>
+  </ThemeProvider>
 );
 
 const HotApp = hot(App);
