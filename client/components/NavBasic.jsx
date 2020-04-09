e9ddf6a8ea9d5102f98c771b91225bf103c48f31
David <David@MacBook-Pro-de-David.local>
2020-04-08 23:18:40 -0500

    Revert "remove title from nav"
    This reverts commit a85068b25e0439573f54bd17ae7cb928ffce5e0b.

--- client/components/NavBasic.jsx
+++ client/components/NavBasic.jsx
@@ -13,7 +13,7 @@ class NavBasic extends React.PureComponent {
   render() {
     return (
       <nav className="nav" title="main-navigation" ref={(node) => { this.node = node; }}>
-        <ul className="nav__items-left">
+        <ul className="nav__items-left" title="project-menu">
           <li className="nav__item-logo">
             <InlineSVG src={logoUrl} alt="p5.js logo" className="svg__logo" />
           </li>
