ce1628cd6964a625963fa03dd7ed4754e50620a5
Cassie Tarakajian <ctarakajian@gmail.com>
2020-04-10 18:30:07 -0400

    Fixes #1268 - fix issue with default protocol of https for local development

--- server/routes/asset.routes.js
+++ server/routes/asset.routes.js
@@ -7,9 +7,10 @@ const router = new Router();
 router.get('/:username/sketches/:project_id/*', getProjectAsset);
 router.get('/full/:project_id/*', getProjectAsset);
 router.get('/:username/full/:project_id/*', getProjectAsset);
+router.get('/present/:project_id/*', getProjectAsset);
+router.get('/:username/present/:project_id/*', getProjectAsset);
 router.get('/embed/:project_id/*', getProjectAsset);
 router.get('/:username/embed/:project_id/*', getProjectAsset);
-router.get('/:username/present/:project_id/*', getProjectAsset);
 
 router.get('/sketches/:project_id/assets/*?', getFileContent);
 
