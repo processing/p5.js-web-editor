24f14f9b5dd1786fd18e064f28afa6028f7abff3
ghalestrilo <ghalestrilo@gmail.com>
2020-04-16 13:17:04 -0300

    :mute: remove unnecessary log

--- client/modules/IDE/actions/files.js
+++ client/modules/IDE/actions/files.js
@@ -135,10 +135,7 @@ export function createFolder(formProps) {
   };
 }
 
-let callcount = 0;
 export function updateFileName(id, name) {
-  callcount += 1;
-  console.log(`called ${callcount} times`);
   return (dispatch) => {
     dispatch(setUnsavedChanges(true));
     dispatch({
