e4888b290698d67eb1374c7db3f7674370fbfa4d
Tirta Rachman <tirtawening@gmail.com>
2020-05-26 17:29:12 -0400

    Add keyboard shortcut note

--- client/modules/IDE/components/KeyboardShortcutModal.jsx
+++ client/modules/IDE/components/KeyboardShortcutModal.jsx
@@ -3,84 +3,83 @@ import { metaKeyName, } from '../../../utils/metaKey';
 
 function KeyboardShortcutModal() {
   return (
-    <ul className="keyboard-shortcuts" title="keyboard shortcuts">
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">{'\u21E7'} + Tab</span>
-        <span>Tidy</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + S
-        </span>
-        <span>Save</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + F
-        </span>
-        <span>Find Text</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + G
-        </span>
-        <span>Find Next Text Match</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + {'\u21E7'} + G
-        </span>
-        <span>Find Previous Text Match</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + D
-        </span>
-        <span>Add Cursor at Next Text Match</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + [
-        </span>
-        <span>Indent Code Left</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + ]
-        </span>
-        <span>Indent Code Right</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + /
-        </span>
-        <span>Comment Line</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + Enter
-        </span>
-        <span>Start Sketch</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + {'\u21E7'} + Enter
-        </span>
-        <span>Stop Sketch</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + {'\u21E7'} + 1
-        </span>
-        <span>Turn on Accessible Output</span>
-      </li>
-      <li className="keyboard-shortcut-item">
-        <span className="keyboard-shortcut__command">
-          {metaKeyName} + {'\u21E7'} + 2
-        </span>
-        <span>Turn off Accessible Output</span>
-      </li>
-    </ul>
+    <div className="keyboard-shortcuts">
+      <div className="keyboard-shortcuts-note">
+        <strong>Note:</strong> our keyboard shortcuts follow <a href="https://shortcuts.design/toolspage-sublimetext.html" target="_blank" rel="noopener noreferrer">Sublime Text shortcuts</a>
+      </div>
+      <ul title="keyboard shortcuts">
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">{'\u21E7'} + Tab</span>
+          <span>Tidy</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + S
+          </span>
+          <span>Save</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + F
+          </span>
+          <span>Find Text</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + G
+          </span>
+          <span>Find Next Text Match</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + {'\u21E7'} + G
+          </span>
+          <span>Find Previous Text Match</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + [
+          </span>
+          <span>Indent Code Left</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + ]
+          </span>
+          <span>Indent Code Right</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + /
+          </span>
+          <span>Comment Line</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + Enter
+          </span>
+          <span>Start Sketch</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + {'\u21E7'} + Enter
+          </span>
+          <span>Stop Sketch</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + {'\u21E7'} + 1
+          </span>
+          <span>Turn on Accessible Output</span>
+        </li>
+        <li className="keyboard-shortcut-item">
+          <span className="keyboard-shortcut__command">
+            {metaKeyName} + {'\u21E7'} + 2
+          </span>
+          <span>Turn off Accessible Output</span>
+        </li>
+      </ul>
+    </div>
   );
 }
 
