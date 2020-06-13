6d90dd2071f81eb2f6e8f78bc3791988ce8d193d
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2020-06-13 12:51:26 +0200

    Do not log in test environment

--- client/utils/getConfig.js
+++ client/utils/getConfig.js
@@ -1,17 +1,24 @@
+function isTestEnvironment() {
+  // eslint-disable-next-line no-use-before-define
+  return getConfig('NODE_ENV', { warn: false }) === 'test';
+}
+
 /**
  * Returns config item from environment
  */
-export default function getConfig(key) {
+function getConfig(key, options = { warn: !isTestEnvironment() }) {
   if (key == null) {
     throw new Error('"key" must be provided to getConfig()');
   }
 
-  const __process = (typeof global !== 'undefined' ? global : window).process;
-  const value = __process.env[key];
+  const env = (typeof global !== 'undefined' ? global : window)?.process?.env || {};
+  const value = env[key];
 
-  if (value == null) {
+  if (value == null && options?.warn !== false) {
     console.warn(`getConfig("${key}") returned null`);
   }
 
   return value;
 }
+
+export default getConfig;
