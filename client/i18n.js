a5e410286cce98d4fdfc966420d076f3586daca6
oruburos <omar.verduga@gmail.com>
2020-06-28 19:52:56 +0100

    Cleaner code in i18n

--- client/i18n.js
+++ client/i18n.js
@@ -4,32 +4,12 @@ import { initReactI18next } from 'react-i18next';
 // import Fetch from 'i18next-fetch-backend';
 // import Fetch from 'i18next-fetch-backend';
 import Backend from 'i18next-http-backend';
-// import axios from 'axios';
-// import commonEn from './locales/en/translations.json';
-// import commonEs from './locales/es/translations.json';
 
 const fallbackLng = ['en'];
 const availableLanguages = ['en', 'es'];
-/* const fileTmp = '/locales/en/translations.json';
-axios.get(fileTmp)
-  .then((response) => {
-    console.log('Datos que traere');
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  }).catch((error) => {
-  // handle error
-    console.log('ERROR AXIOS ASA AXIOS');
-    console.log(error);
-  }); */
-
 
 const options = {
-  // loadPath: '/locales/{{lng}}/translations.json',
   loadPath: '/locales/{{lng}}/translations.json',
-  // loadPath: fileTmp,
   requestOptions: { // used for fetch, can also be a function (payload) => ({ method: 'GET' })
     mode: 'no-cors'
   },
