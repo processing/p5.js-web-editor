78ec304e1572819874ee4631ef8560539b138eec
ghalestrilo <ghalestrilo@gmail.com>
2020-06-29 14:51:40 -0300

    :ok_hand: remove eslint-disable

--- client/modules/Mobile/MobileSketchView.jsx
+++ client/modules/Mobile/MobileSketchView.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
