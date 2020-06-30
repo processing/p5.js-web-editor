b96b9d350a857af461ddc4b732cdbe00b20b67ee
ghalestrilo <ghalestrilo@gmail.com>
2020-06-30 16:41:26 -0300

    :ok_hand: use remSize on IconButton

--- client/components/mobile/IconButton.jsx
+++ client/components/mobile/IconButton.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Button from '../../common/Button';
+import { remSize } from '../../theme';
 
 const ButtonWrapper = styled(Button)`
-width: 3rem;
+width: ${remSize(48)};
 > svg {
   width: 100%;
   height: 100%;
