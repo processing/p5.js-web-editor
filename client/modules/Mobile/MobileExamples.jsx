57ee6f0b8b21e4d9c583cfb9044bea994188bd9c
ghalestrilo <ghalestrilo@gmail.com>
2020-07-28 11:33:38 -0300

    :construction: mount List components

--- client/modules/Mobile/MobileExamples.jsx
+++ client/modules/Mobile/MobileExamples.jsx
@@ -7,9 +7,13 @@ import IconButton from '../../components/mobile/IconButton';
 import { ExitIcon } from '../../common/icons';
 import Footer from '../../components/mobile/Footer';
 import { prop, remSize } from '../../theme';
+import SketchList from '../IDE/components/SketchList';
+import CollectionList from '../IDE/components/CollectionList';
+import AssetList from '../IDE/components/AssetList';
+import Content from './MobileViewContent';
 
 const FooterTab = styled.div`
-  background: ${props => prop(`MobilePanel.default.${props.selected ? 'background' : 'foreground'}`)};
+  background: ${props => prop(props.selected ? 'backgroundColor' : 'MobilePanel.default.foreground')};
   color: ${props => prop(`MobilePanel.default.${props.selected ? 'foreground' : 'background'}`)};
   padding: ${remSize(16)};
   width: 100%;
@@ -19,24 +23,49 @@ const FooterTab = styled.div`
 const FooterTabContainer = styled.div`
   display: flex;
   
-  h3 { text-align: center; width: 100%;}
+  h3 { text-align: center; width: 100%; }
 `;
 
+// switch (tabKey) {
+//   case TabKey.assets:
+//     return <AssetList key={username} username={username} />;
+//   case TabKey.collections:
+//     return <CollectionList key={username} username={username} />;
+//   case TabKey.sketches:
+//   default:
+//     return <SketchList key={username} username={username} />;
+// }
+
+const Panels = {
+  Sketches: () => <SketchList />,
+  Collections: () => <CollectionList />,
+  Assets: () => <AssetList />
+};
 
 const MobileExamples = () => {
-  const tabs = ['Sketches', 'Collections', 'Assets'];
-  const [selected, selectTab] = useState(tabs[0]);
+  // const tabs = ['Sketches', 'Collections', 'Assets'];
+  const Tabs = Object.keys(Panels);
+  const [selected, selectTab] = useState(Tabs[0]);
   return (
     <Screen fullscreen>
       <Header inverted title="My Stuff">
         <IconButton to="/mobile" icon={ExitIcon} aria-label="Return to ide view" />
       </Header>
 
+
+      <Content>
+        {Panels[selected] && Panels[selected]()}
+      </Content>
+
+
       <Footer>
         <FooterTabContainer>
-
-          {tabs.map(tab => (
-            <FooterTab selected={tab === selected} onClick={() => selectTab(tab)}>
+          {Tabs.map(tab => (
+            <FooterTab
+              key={`tab-${tab}`}
+              selected={tab === selected}
+              onClick={() => selectTab(tab)}
+            >
               <h3>{tab}</h3>
             </FooterTab>))
           }
---
+++ client/modules/Mobile/MobileViewContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import { remSize } from '../../theme';
+
+
+export default styled.div`
+  z-index: 0;
+  margin-top: ${remSize(68)};
+`;
