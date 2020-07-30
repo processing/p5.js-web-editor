5e8d4813a66bb0d643a68f8fab829bfb8287d818
Cassie Tarakajian <ctarakajian@gmail.com>
2020-07-30 13:45:13 -0400

    [#1396] Update p5.js version to 1.1.9

--- server/scripts/examples.js
+++ server/scripts/examples.js
@@ -11,8 +11,8 @@ const defaultHTML =
 `<!DOCTYPE html>
 <html lang="en">
   <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.1/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.1/addons/p5.sound.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/addons/p5.sound.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8" />
   </head>
