b27f1acb06eba386fe9d1fb50d01503679f6182f
ghalestrilo <ghalestrilo@gmail.com>
2020-08-03 16:34:34 -0300

    :ambulance: restore devtools sidebar

--- client/modules/App/App.jsx
+++ client/modules/App/App.jsx
@@ -34,8 +34,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        {/* FIXME: Remove false */}
-        {false && this.state.isMounted && !window.devToolsExtension && getConfig('NODE_ENV') === 'development' && <DevTools />}
+        {this.state.isMounted && !window.devToolsExtension && getConfig('NODE_ENV') === 'development' && <DevTools />}
         {this.props.children}
       </div>
     );
