222b6d8c627f3827998a5c504b3ad20e8b30c3bb
ghalestrilo <ghalestrilo@gmail.com>
2020-08-11 16:45:32 -0300

    :ok_hand: remove gap between header and search bar

--- client/modules/Mobile/MobileViewContent.jsx
+++ client/modules/Mobile/MobileViewContent.jsx
@@ -6,5 +6,5 @@ import { remSize } from '../../theme';
 export default styled.div`
   /* Dashboard Styles */
   z-index: 0;
-  margin-top: ${props => remSize(props.slimheader ? 50 : 68)};
+  margin-top: ${props => remSize(props.slimheader ? 49 : 68)};
 `;
