27484623b5213d3f4a4191f6ed8799bec383271f
oruburos <omar.verduga@gmail.com>
2020-08-13 12:56:10 +0100

    Change from i18t to this.props. Delete unused imports.

--- client/modules/User/components/APIKeyForm.jsx
+++ client/modules/User/components/APIKeyForm.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import i18next from 'i18next';
 import Button from '../../../common/Button';
 import { PlusIcon } from '../../../common/icons';
 import CopyableInput from '../../IDE/components/CopyableInput';
