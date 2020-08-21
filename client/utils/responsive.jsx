f4d654a08f2bbb9d1da6042c6aad3f9cc4e5765d
ghalestrilo <ghalestrilo@gmail.com>
2020-08-21 18:07:41 -0300

    :sparkles: make forceDesktop work

--- client/utils/responsive.jsx
+++ client/utils/responsive.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import MediaQuery from 'react-responsive';
 import ResponsiveForm from '../modules/User/components/ResponsiveForm';
 
 export const mobileEnabled = () => (window.process.env.MOBILE_ENABLED === true);
 
-
 /** createMobileFirst: Receives the store, and creates a function that chooses between two components,
  * aimed at mobile and desktop resolutions, respectively.
  * The created function returns a Component (props => jsx)
  */
-export const createMobileFirst = store => (MobileComponent, Fallback) => props => (
-  <MediaQuery minWidth={770}>
-    {matches => ((matches || (store && store.getState().editorAccessibility.forceDesktop) || (!mobileEnabled()))
-      ? <Fallback {...props} />
-      : <MobileComponent {...props} />)}
-  </MediaQuery>);
+export const createMobileFirst = store => (MobileComponent, Fallback) => (props) => {
+  const { forceDesktop } = useSelector(state => state.editorAccessibility);
+  return (
+    <MediaQuery minWidth={770}>
+      {matches => ((matches || (store && forceDesktop) || (!mobileEnabled()))
+        ? <Fallback {...props} />
+        : <MobileComponent {...props} />)}
+    </MediaQuery>
+  );
+};
 
 export const responsiveForm = DesktopComponent => props => (
   <ResponsiveForm>
