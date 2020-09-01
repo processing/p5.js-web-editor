328616fe2bf7508aad537f415fae3462359d5fee
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-09-01 16:53:46 +0530

    Fixed bug in replace using regexp

--- client/utils/codemirror-search.js
+++ client/utils/codemirror-search.js
@@ -280,7 +280,6 @@ export default function(CodeMirror) {
   function doFindAndReplace(cm, rev, persistent, immediate, ignoreQuery, replaceOpened) {
     var state = getSearchState(cm);
     if (!ignoreQuery && state.query) {
-      console.log('Here');
       return findNext(cm, rev);
     }
     var q = cm.getSelection() || state.lastQuery;
@@ -303,57 +302,6 @@ export default function(CodeMirror) {
         })
       };
       persistentDialog(cm, queryDialog, q, searchNext, replaceOpened, function(event, query) {
-        var keyName = CodeMirror.keyName(event)
-        var cmd = CodeMirror.keyMap[cm.getOption("keyMap")][keyName]
-        if (!cmd) cmd = cm.getOption('extraKeys')[keyName]
-        if (cmd == "findNext" || cmd == "findPrev" ||
-          cmd == "findPersistentNext" || cmd == "findPersistentPrev") {
-          CodeMirror.e_stop(event);
-          startSearch(cm, getSearchState(cm), query);
-          cm.execCommand(cmd);
-        } else if (cmd == "find" || cmd == "findPersistent") {
-          CodeMirror.e_stop(event);
-          searchNext(query, event);
-        }
-      });
-      if (immediate && q) {
-        startSearch(cm, state, q);
-        findNext(cm, rev);
-      }
-    } else {
-      dialog(cm, queryDialog, "Search for:", q, function(query) {
-        if (query && !state.query) cm.operation(function() {
-          startSearch(cm, state, query);
-          state.posFrom = state.posTo = cm.getCursor();
-          findNext(cm, rev);
-        });
-      });
-    }
-  }
-
-  function doSearch(cm, rev, persistent, immediate, ignoreQuery) {
-    var state = getSearchState(cm);
-    if (!ignoreQuery && state.query) return findNext(cm, rev);
-    var q = cm.getSelection() || state.lastQuery;
-    if (persistent && cm.openDialog) {
-      var hiding = null
-      var searchNext = function(query, event) {
-        CodeMirror.e_stop(event);
-        if (!query) return;
-        if (query != state.queryText) {
-          startSearch(cm, state, query);
-          state.posFrom = state.posTo = cm.getCursor();
-        }
-        if (hiding) hiding.style.opacity = 1
-        findNext(cm, event.shiftKey, function(_, to) {
-          var dialog
-          if (to.line < 3 && document.querySelector &&
-            (dialog = cm.display.wrapper.querySelector(".CodeMirror-dialog")) &&
-            dialog.getBoundingClientRect().bottom - 4 > cm.cursorCoords(to, "window").top)
-            (hiding = dialog).style.opacity = .4
-        })
-      };
-      persistentDialog(cm, queryDialog, q, searchNext, function(event, query) {
         var keyName = CodeMirror.keyName(event)
         var cmd = CodeMirror.keyMap[cm.getOption("keyMap")][keyName]
         if (!cmd) cmd = cm.getOption('extraKeys')[keyName]
@@ -433,8 +381,7 @@ export default function(CodeMirror) {
     prevDialog.parentNode.removeChild(prevDialog);
     cm.focus();
     if (cm.getOption("readOnly")) return;
-    var query = cm.getSelection() || state.lastQuery;
-
+    var query = parseQuery(queryText, state);
     if (all) {
       replaceAll(cm, query, withText)
     } else {
@@ -542,8 +489,8 @@ export default function(CodeMirror) {
   CodeMirror.commands.findPersistent = function(cm) {doFindAndReplace(cm, false, true, false, true, false);};
   CodeMirror.commands.findPersistentNext = function(cm) {doFindAndReplace(cm, false, true, false, true, false);};
   CodeMirror.commands.findPersistentPrev = function(cm) {doFindAndReplace(cm, false, true, false, true, false);};
-  CodeMirror.commands.findNext = doSearch;
-  CodeMirror.commands.findPrev = function(cm) {doSearch(cm, true);};
+  CodeMirror.commands.findNext = doFindAndReplace;
+  CodeMirror.commands.findPrev = function(cm) {doFindAndReplace(cm, true);};
   CodeMirror.commands.clearSearch = clearSearch;
   CodeMirror.commands.replace = function(cm) { doFindAndReplace(cm, false, true, false, true, true); };
   CodeMirror.commands.replaceAll = function(cm) { doFindAndReplace(cm, false, true, false, true, true); };
