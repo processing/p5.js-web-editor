cf0cd38269158be95384e0fabe9b232d51045dc1
Ruben van de Ven <git@rubenvandeven.com>
2020-09-04 22:08:38 +0200

    Fix #358 - migrate to S3 policy v4 to support AWS4-HMAC-SHA256

--- server/controllers/aws.controller.js
+++ server/controllers/aws.controller.js
@@ -1,5 +1,5 @@
 import uuid from 'node-uuid';
-import policy from 's3-policy';
+import S3Policy from 's3-policy-v4';
 import s3 from '@auth0/s3';
 import { getProjectsForUserId } from './project.controller';
 import { findUserByUsername } from './user.controller';
@@ -80,22 +80,19 @@ export function signS3(req, res) {
   }
   const fileExtension = getExtension(req.body.name);
   const filename = uuid.v4() + fileExtension;
-  const acl = 'public-read';
-  const p = policy({
+  const acl = 'private';
+  const policy = S3Policy.generate({
     acl,
-    secret: process.env.AWS_SECRET_KEY,
-    length: 5000000, // in bytes?
-    bucket: process.env.S3_BUCKET,
     key: filename,
-    expires: new Date(Date.now() + 60000),
+    bucket: process.env.S3_BUCKET,
+    contentType: req.body.type,
+    region: process.env.AWS_REGION,
+    accessKey: process.env.AWS_ACCESS_KEY,
+    secretKey: process.env.AWS_SECRET_KEY,
+    // metadata: {'x-amz-meta-lat': '41.891',...} (optional)
+    metadata: []
   });
-  const result = {
-    AWSAccessKeyId: process.env.AWS_ACCESS_KEY,
-    key: `${req.body.userId}/${filename}`,
-    policy: p.policy,
-    signature: p.signature
-  };
-  res.json(result);
+  res.json(policy);
 }
 
 export function copyObjectInS3(url, userId) {
