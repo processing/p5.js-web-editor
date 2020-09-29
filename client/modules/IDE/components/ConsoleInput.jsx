ac5c376500c20c5745c1ce9e45a373e8493c9e27
Cassie Tarakajian <ctarakajian@gmail.com>
2020-09-29 13:09:53 -0400

    Remove unused variables, add return at end of files

--- client/modules/IDE/components/ConsoleInput.jsx
+++ client/modules/IDE/components/ConsoleInput.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CodeMirror from 'codemirror';
 import { Encode } from 'console-feed';
 
