f76f88a4020c21186a30168436a5dd9696fc459c
Cassie Tarakajian <ctarakajian@gmail.com>
2020-09-29 15:25:42 -0400

    Revert "Revert "Make console interactive""

--- client/utils/previewEntry.js
+++ client/utils/previewEntry.js
@@ -1,5 +1,6 @@
 import loopProtect from 'loop-protect';
-import { Hook } from 'console-feed';
+import { Hook, Decode, Encode } from 'console-feed';
+import evaluateExpression from './evaluateExpression';
 
 window.loopProtect = loopProtect;
 
@@ -7,13 +8,36 @@ const consoleBuffer = [];
 const LOGWAIT = 500;
 Hook(window.console, (log) => {
   consoleBuffer.push({
-    log,
-    source: 'sketch'
+    log
   });
 });
 setInterval(() => {
   if (consoleBuffer.length > 0) {
-    window.parent.postMessage(consoleBuffer, '*');
+    const message = {
+      messages: consoleBuffer,
+      source: 'sketch'
+    };
+    window.parent.postMessage(message, window.origin);
     consoleBuffer.length = 0;
   }
 }, LOGWAIT);
+
+function handleMessageEvent(e) {
+  if (window.origin !== e.origin) return;
+  const { data } = e;
+  const { source, messages } = data;
+  if (source === 'console' && Array.isArray(messages)) {
+    const decodedMessages = messages.map(message => Decode(message.log));
+    decodedMessages.forEach((message) => {
+      const { data: args } = message;
+      const { result, error } = evaluateExpression(args);
+      const resultMessages = [{ log: Encode({ method: error ? 'error' : 'result', data: [result] }) }];
+      window.parent.postMessage({
+        messages: resultMessages,
+        source: 'sketch'
+      }, window.origin);
+    });
+  }
+}
+
+window.addEventListener('message', handleMessageEvent);
