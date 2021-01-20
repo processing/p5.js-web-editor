ccf1e13117fd7c2a024af00aea4832095a130e41
Mark O Grady <mark.o'grady@broadbean.com>
2021-01-20 17:03:29 +0000

    add missing isUserOwner key

--- client/components/__test__/Nav.test.jsx
+++ client/components/__test__/Nav.test.jsx
@@ -48,7 +48,8 @@ describe('Nav', () => {
     },
     t: jest.fn(),
     setLanguage: jest.fn(),
-    language: 'en-US'
+    language: 'en-US',
+    isUserOwner: true
   };
 
   it('renders correctly', () => {
