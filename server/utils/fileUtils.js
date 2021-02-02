f3754125aab16bd6e22f49ae6664215c2bbac26a
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-02-02 19:47:36 +0530

    [#1741]-Added xml to Regex

--- server/utils/fileUtils.js
+++ server/utils/fileUtils.js
@@ -38,12 +38,13 @@ export const fileExtensionsArray = [
   'mov',
   'vert',
   'frag',
-  'bin'
+  'bin',
+  'xml'
 ];
 
 export const mimeTypes = `image/*,audio/*,text/javascript,text/html,text/css,
 application/json,application/x-font-ttf,application/x-font-truetype,text/plain,
-text/csv,.obj,video/webm,video/ogg,video/quicktime,video/mp4`;
+text/csv,.obj,video/webm,video/ogg,video/quicktime,video/mp4,application/xml`;
 
 export const fileExtensions = fileExtensionsArray
   .map((ext) => `.${ext}`)
@@ -64,10 +65,10 @@ export const MEDIA_FILE_QUOTED_REGEX = new RegExp(
 
 export const STRING_REGEX = /(['"])((\\\1|.)*?)\1/gm;
 // these are files that have to be linked to with a blob url
-export const PLAINTEXT_FILE_REGEX = /.+\.(json|txt|csv|vert|frag|tsv)$/i;
+export const PLAINTEXT_FILE_REGEX = /.+\.(json|txt|csv|vert|frag|tsv|xml)$/i;
 // these are files that users would want to edit as text (maybe svg should be here?)
-export const TEXT_FILE_REGEX = /.+\.(json|txt|csv|tsv|vert|frag|js|css|html|htm|jsx)$/i;
+export const TEXT_FILE_REGEX = /.+\.(json|txt|csv|tsv|vert|frag|js|css|html|htm|jsx|xml)$/i;
 export const NOT_EXTERNAL_LINK_REGEX = /^(?!(http:\/\/|https:\/\/))/;
 export const EXTERNAL_LINK_REGEX = /^(http:\/\/|https:\/\/)/;
 
-export const CREATE_FILE_REGEX = /.+\.(json|txt|csv|tsv|js|css|frag|vert)$/i;
+export const CREATE_FILE_REGEX = /.+\.(json|txt|csv|tsv|js|css|frag|vert|xml)$/i;
