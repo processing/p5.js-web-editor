445115d5e0e9fa4decb894d8aa5823dbf09752cc
Ajaya1000 <ajaymati1000@gmail.com>
2021-03-15 23:40:32 +0530

    [#1760]  About Component scss to styled component

--- client/modules/IDE/components/About.jsx
+++ client/modules/IDE/components/About.jsx
@@ -1,103 +1,143 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 import SquareLogoIcon from '../../../images/p5js-square-logo.svg';
 // import PlayIcon from '../../../images/play.svg';
 import AsteriskIcon from '../../../images/p5-asterisk.svg';
+import { remSize } from '../../../theme';
+
+const AboutLogo = styled(SquareLogoIcon)`
+  & path {
+    fill: ${({ theme }) => theme.logoColor.default.fill};
+  }
+`;
+
+const AboutContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding-top: ${remSize(17)};
+  padding-right: ${remSize(78)};
+  padding-bottom: ${remSize(20)};
+  padding-left: ${remSize(20)};
+  width: ${remSize(720)};
+`;
+
+const AboutContentColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const AboutContentColumnTitle = styled.h3`
+  font-size: ${remSize(21)};
+  padding-left: ${remSize(17)};
+`;
+
+const AboutContentColumnAsterisk = styled(AsteriskIcon)`
+  &&& {
+    padding-right: ${remSize(5)};
+    & path {
+      fill: ${({ theme }) => theme.logoColor.default.fill};
+      stroke: ${({ theme }) => theme.logoColor.default.stroke};
+    }
+  }
+`;
+
+const AboutContentColumnList = styled.p`
+  padding-top: ${remSize(10)};
+  font-size: ${remSize(16)};
+`;
+
+const AboutFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: ${remSize(18)};
+  padding-right: ${remSize(20)};
+  padding-bottom: ${remSize(21)};
+  padding-left: ${remSize(291)};
+  width: 100%;
+`;
+
+const AboutFooterList = styled.p`
+  padding-top: ${remSize(12)};
+`;
 
 function About(props) {
   const { t } = useTranslation();
   return (
-    <div className="about__content">
+    <AboutContentContainer>
       <Helmet>
         <title> {t('About.TitleHelmet')} </title>
       </Helmet>
-      <div className="about__content-column">
-        <SquareLogoIcon
-          className="about__logo"
+      <AboutContentColumn>
+        <AboutLogo
           role="img"
           aria-label={t('Common.p5logoARIA')}
           focusable="false"
         />
-      </div>
-      <div className="about__content-column">
-        <h3 className="about__content-column-title">{t('About.NewP5')}</h3>
-        <p className="about__content-column-list">
+      </AboutContentColumn>
+      <AboutContentColumn>
+        <AboutContentColumnTitle>{t('About.NewP5')}</AboutContentColumnTitle>
+        <AboutContentColumnList>
           <a
             href="https://p5js.org/examples/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AsteriskIcon
-              className="about__content-column-asterisk"
-              aria-hidden="true"
-              focusable="false"
-            />
+            <AboutContentColumnAsterisk aria-hidden="true" focusable="false" />
             {t('About.Examples')}
           </a>
-        </p>
-        <p className="about__content-column-list">
+        </AboutContentColumnList>
+        <AboutContentColumnList>
           <a
             href="https://p5js.org/learn/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AsteriskIcon
-              className="about__content-column-asterisk"
-              aria-hidden="true"
-              focusable="false"
-            />
+            <AboutContentColumnAsterisk aria-hidden="true" focusable="false" />
             {t('About.Learn')}
           </a>
-        </p>
-      </div>
-      <div className="about__content-column">
-        <h3 className="about__content-column-title">{t('About.Resources')}</h3>
-        <p className="about__content-column-list">
+        </AboutContentColumnList>
+      </AboutContentColumn>
+      <AboutContentColumn>
+        <AboutContentColumnTitle>
+          {t('About.Resources')}
+        </AboutContentColumnTitle>
+        <AboutContentColumnList>
           <a
             href="https://p5js.org/libraries/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AsteriskIcon
-              className="about__content-column-asterisk"
-              aria-hidden="true"
-              focusable="false"
-            />
+            <AboutContentColumnAsterisk aria-hidden="true" focusable="false" />
             {t('About.Libraries')}
           </a>
-        </p>
-        <p className="about__content-column-list">
+        </AboutContentColumnList>
+        <AboutContentColumnList>
           <a
             href="https://p5js.org/reference/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AsteriskIcon
-              className="about__content-column-asterisk"
-              aria-hidden="true"
-              focusable="false"
-            />
+            <AboutContentColumnAsterisk aria-hidden="true" focusable="false" />
             {t('Nav.Help.Reference')}
           </a>
-        </p>
-        <p className="about__content-column-list">
+        </AboutContentColumnList>
+        <AboutContentColumnList>
           <a
             href="https://discourse.processing.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <AsteriskIcon
-              className="about__content-column-asterisk"
-              aria-hidden="true"
-              focusable="false"
-            />
+            <AboutContentColumnAsterisk aria-hidden="true" focusable="false" />
             {t('About.Forum')}
           </a>
-        </p>
-      </div>
-      <div className="about__footer">
-        <p className="about__footer-list">
+        </AboutContentColumnList>
+      </AboutContentColumn>
+      <AboutFooter>
+        <AboutFooterList>
           <a
             href="https://github.com/processing/p5.js-web-editor"
             target="_blank"
@@ -105,8 +145,8 @@ function About(props) {
           >
             {t('About.Contribute')}
           </a>
-        </p>
-        <p className="about__footer-list">
+        </AboutFooterList>
+        <AboutFooterList>
           <a
             href="https://github.com/processing/p5.js-web-editor/issues/new"
             target="_blank"
@@ -114,8 +154,8 @@ function About(props) {
           >
             {t('About.Report')}
           </a>
-        </p>
-        <p className="about__footer-list">
+        </AboutFooterList>
+        <AboutFooterList>
           <a
             href="https://twitter.com/p5xjs?lang=en"
             target="_blank"
@@ -123,9 +163,9 @@ function About(props) {
           >
             Twitter
           </a>
-        </p>
-      </div>
-    </div>
+        </AboutFooterList>
+      </AboutFooter>
+    </AboutContentContainer>
   );
 }
 
