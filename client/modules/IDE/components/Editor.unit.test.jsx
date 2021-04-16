71751586ef4f77d2ea2dde64d9db6a849c15c8e7
Connie Ye <constanceye1316@gmail.com>
2021-04-16 14:51:03 -0400

    fix linting error

--- client/modules/IDE/components/Editor.unit.test.jsx
+++ client/modules/IDE/components/Editor.unit.test.jsx
@@ -3,13 +3,7 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { act } from 'react-dom/test-utils';
 import Editor from './Editor';
-import {
-  reduxRender,
-  fireEvent,
-  screen,
-  within,
-  prettyDOM
-} from '../../../test-utils';
+import { reduxRender } from '../../../test-utils';
 import { initialTestState } from '../../../testData/testReduxStore';
 
 jest.mock('../../../i18n');
