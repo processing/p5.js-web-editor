b13e8c8ad7170616878d8b06e5ee31b7bb1b10e5
Abhinav Srinivas <abhinav14863@gmail.com>
2023-03-11 12:26:14 +0530

    fixed linting error

--- client/modules/IDE/components/Console.jsx
+++ client/modules/IDE/components/Console.jsx
@@ -267,7 +267,7 @@ const Console = () => {
           style={{ fontSize }}
         >
           <ConsoleFeed
-            variant={theme === "light" ? "light" : "dark"}
+            variant={theme === 'light' ? 'light' : 'dark'}
             styles={getConsoleFeedStyle(theme, fontSize)}
             logs={consoleEvents}
             key={`${theme}-${fontSize}`}
