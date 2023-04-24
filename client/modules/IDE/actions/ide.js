fd686f2a6b77f03e78779e3bdc1610bc8bace88b
Linda Paiste <lindapaiste@gmail.com>
2023-04-24 12:00:03 -0500

    Refactor IDE reducer and actions using Redux Toolkit.

--- client/modules/IDE/actions/ide.js
+++ client/modules/IDE/actions/ide.js
@@ -2,49 +2,48 @@ import * as ActionTypes from '../../../constants';
 import { clearConsole } from './console';
 import { dispatchMessage, MessageTypes } from '../../../utils/dispatcher';
 
-export function startVisualSketch() {
-  return {
-    type: ActionTypes.START_SKETCH
-  };
-}
+import { ideActions } from '../reducers/ide';
 
-export function stopVisualSketch() {
-  return {
-    type: ActionTypes.STOP_SKETCH
-  };
-}
+// TODO: refactor actions which are only used internally by other actions.
+const {
+  startVisualSketch,
+  stopVisualSketch,
+  startRefreshSketch,
+  startAccessibleOutput,
+  stopAccessibleOutput,
+  showShareModal: showShareModalInternal
+} = ideActions;
 
-export function startRefreshSketch() {
-  return {
-    type: ActionTypes.START_SKETCH_REFRESH
-  };
-}
+export const {
+  openUploadFileModal,
+  closeUploadFileModal,
+  hideRuntimeErrorWarning,
+  showRuntimeErrorWarning,
+  setUnsavedChanges,
+  endSketchRefresh, // TODO: export is not actually needed
+  newFile,
+  closeNewFileModal,
+  newFolder,
+  closeNewFolderModal,
+  expandSidebar,
+  collapseSidebar,
+  expandConsole,
+  collapseConsole,
+  openPreferences,
+  closePreferences,
+  openProjectOptions,
+  closeProjectOptions,
+  closeShareModal,
+  showKeyboardShortcutModal,
+  closeKeyboardShortcutModal,
+  showErrorModal,
+  hideErrorModal,
+  setPreviousPath,
+  justOpenedProject,
+  resetJustOpenedProject
+} = ideActions;
 
-export function startSketchAndRefresh() {
-  return (dispatch) => {
-    dispatch(startVisualSketch());
-    dispatch(startRefreshSketch());
-  };
-}
-
-export function endSketchRefresh() {
-  return {
-    type: ActionTypes.END_SKETCH_REFRESH
-  };
-}
-
-export function startAccessibleOutput() {
-  return {
-    type: ActionTypes.START_ACCESSIBLE_OUTPUT
-  };
-}
-
-export function stopAccessibleOutput() {
-  return {
-    type: ActionTypes.STOP_ACCESSIBLE_OUTPUT
-  };
-}
-
+// TODO: move to /files
 export function setSelectedFile(fileId) {
   return {
     type: ActionTypes.SET_SELECTED_FILE,
@@ -52,199 +51,27 @@ export function setSelectedFile(fileId) {
   };
 }
 
+// TODO: move to /files
 export function resetSelectedFile(previousId) {
   return (dispatch, getState) => {
     const state = getState();
     const newId = state.files.find(
       (file) => file.name !== 'root' && file.id !== previousId
     ).id;
-    dispatch({
-      type: ActionTypes.SET_SELECTED_FILE,
-      selectedFile: newId
-    });
-  };
-}
-
-export function newFile(parentId) {
-  return {
-    type: ActionTypes.SHOW_MODAL,
-    parentId
-  };
-}
-
-export function closeNewFileModal() {
-  return {
-    type: ActionTypes.HIDE_MODAL
-  };
-}
-
-export function openUploadFileModal(parentId) {
-  return {
-    type: ActionTypes.OPEN_UPLOAD_FILE_MODAL,
-    parentId
-  };
-}
-
-export function closeUploadFileModal() {
-  return {
-    type: ActionTypes.CLOSE_UPLOAD_FILE_MODAL
-  };
-}
-
-export function expandSidebar() {
-  return {
-    type: ActionTypes.EXPAND_SIDEBAR
-  };
-}
-
-export function collapseSidebar() {
-  return {
-    type: ActionTypes.COLLAPSE_SIDEBAR
-  };
-}
-
-export function expandConsole() {
-  return {
-    type: ActionTypes.EXPAND_CONSOLE
-  };
-}
-
-export function collapseConsole() {
-  return {
-    type: ActionTypes.COLLAPSE_CONSOLE
-  };
-}
-
-export function openPreferences() {
-  return {
-    type: ActionTypes.OPEN_PREFERENCES
-  };
-}
-
-export function closePreferences() {
-  return {
-    type: ActionTypes.CLOSE_PREFERENCES
-  };
-}
-
-export function openProjectOptions() {
-  return {
-    type: ActionTypes.OPEN_PROJECT_OPTIONS
-  };
-}
-
-export function closeProjectOptions() {
-  return {
-    type: ActionTypes.CLOSE_PROJECT_OPTIONS
-  };
-}
-
-export function newFolder(parentId) {
-  return {
-    type: ActionTypes.SHOW_NEW_FOLDER_MODAL,
-    parentId
-  };
-}
-
-export function closeNewFolderModal() {
-  return {
-    type: ActionTypes.CLOSE_NEW_FOLDER_MODAL
+    dispatch(setSelectedFile(newId));
   };
 }
 
 export function showShareModal(projectId, projectName, ownerUsername) {
   return (dispatch, getState) => {
     const { project, user } = getState();
-    dispatch({
-      type: ActionTypes.SHOW_SHARE_MODAL,
-      payload: {
+    dispatch(
+      showShareModalInternal({
         shareModalProjectId: projectId || project.id,
         shareModalProjectName: projectName || project.name,
         shareModalProjectUsername: ownerUsername || user.username
-      }
-    });
-  };
-}
-
-export function closeShareModal() {
-  return {
-    type: ActionTypes.CLOSE_SHARE_MODAL
-  };
-}
-
-export function showKeyboardShortcutModal() {
-  return {
-    type: ActionTypes.SHOW_KEYBOARD_SHORTCUT_MODAL
-  };
-}
-
-export function closeKeyboardShortcutModal() {
-  return {
-    type: ActionTypes.CLOSE_KEYBOARD_SHORTCUT_MODAL
-  };
-}
-
-export function setUnsavedChanges(value) {
-  return {
-    type: ActionTypes.SET_UNSAVED_CHANGES,
-    value
-  };
-}
-
-export function detectInfiniteLoops(message) {
-  return {
-    type: ActionTypes.DETECT_INFINITE_LOOPS,
-    message
-  };
-}
-
-export function resetInfiniteLoops() {
-  return {
-    type: ActionTypes.RESET_INFINITE_LOOPS
-  };
-}
-
-export function justOpenedProject() {
-  return {
-    type: ActionTypes.JUST_OPENED_PROJECT
-  };
-}
-
-export function resetJustOpenedProject() {
-  return {
-    type: ActionTypes.RESET_JUST_OPENED_PROJECT
-  };
-}
-
-export function setPreviousPath(path) {
-  return {
-    type: ActionTypes.SET_PREVIOUS_PATH,
-    path
-  };
-}
-
-export function showErrorModal(modalType) {
-  return {
-    type: ActionTypes.SHOW_ERROR_MODAL,
-    modalType
-  };
-}
-
-export function hideErrorModal() {
-  return {
-    type: ActionTypes.HIDE_ERROR_MODAL
-  };
-}
-
-export function hideRuntimeErrorWarning() {
-  return {
-    type: ActionTypes.HIDE_RUNTIME_ERROR_WARNING
-  };
-}
-
-export function showRuntimeErrorWarning() {
-  return {
-    type: ActionTypes.SHOW_RUNTIME_ERROR_WARNING
+      })
+    );
   };
 }
 
@@ -269,11 +96,13 @@ export function startSketch() {
   };
 }
 
+// TODO: does this need to call dispatchMessage like in startSketch? Should it call startSketch internally?
 export function startAccessibleSketch() {
   return (dispatch) => {
     dispatch(clearConsole());
     dispatch(startAccessibleOutput());
-    dispatch(startSketchAndRefresh());
+    dispatch(startVisualSketch());
+    dispatch(startRefreshSketch());
   };
 }
 
@@ -287,6 +116,7 @@ export function stopSketch() {
   };
 }
 
+// TODO: move to /files
 export function createError(error) {
   return {
     type: ActionTypes.ERROR,
