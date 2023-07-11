cf9c0afeb136ee391cb2c6d354af4f26449c8a7e
raclim <43053081+raclim@users.noreply.github.com>
2023-07-11 11:05:39 -0400

    update imports to point to mirror

--- client/modules/IDE/components/ConsoleInput.jsx
+++ client/modules/IDE/components/ConsoleInput.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import CodeMirror from 'codemirror';
-import { Encode } from 'console-feed';
+import { Encode } from '@nicksrandall/console-feed';
 
 import RightArrowIcon from '../../../images/right-arrow.svg';
 import { dispatchMessage, MessageTypes } from '../../../utils/dispatcher';
