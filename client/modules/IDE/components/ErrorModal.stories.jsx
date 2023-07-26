f9c4800586baac3a47762f2e469bf89689494c61
Alexis <alexis.hope@myzeller.com>
2023-07-26 10:59:49 +1000

    docs(ErrorModal): remove redundant config

--- client/modules/IDE/components/ErrorModal.stories.jsx
+++ client/modules/IDE/components/ErrorModal.stories.jsx
@@ -1,47 +1,30 @@
-import React from 'react';
-
 import ErrorModal from './ErrorModal';
 
 export default {
   title: 'IDE/ErrorModal',
   component: ErrorModal,
   argTypes: {
-    type: {
-      options: [
-        'forceAuthentication',
-        'staleSession',
-        'staleProject',
-        'oauthError'
-      ],
-      control: { type: 'select' }
-    },
-    service: {
-      options: ['google', 'github'],
-      control: { type: 'select' }
-    },
     closeModal: { action: 'closed' }
   }
 };
 
-const Template = (args) => <ErrorModal {...args} />;
-
-export const ForceAuthenticationErrorModal = Template.bind({});
-ForceAuthenticationErrorModal.args = {
-  type: 'forceAuthentication'
+export const ForceAuthenticationErrorModal = {
+  args: {
+    type: 'forceAuthentication'
+  }
 };
-
-export const StaleSessionErrorModal = Template.bind({});
-StaleSessionErrorModal.args = {
-  type: 'staleSession'
+export const StaleSessionErrorModal = {
+  args: {
+    type: 'staleSession'
+  }
 };
-
-export const StaleProjectErrorModal = Template.bind({});
-StaleProjectErrorModal.args = {
-  type: 'staleProject'
+export const StaleProjectErrorModal = {
+  args: {
+    type: 'staleProject'
+  }
 };
-
-export const OauthErrorModal = Template.bind({});
-OauthErrorModal.args = {
-  type: 'oauthError',
-  service: 'google'
+export const OauthErrorModal = {
+  args: {
+    type: 'oauthError'
+  }
 };
