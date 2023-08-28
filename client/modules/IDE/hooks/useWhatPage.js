8b2e3df3e5b0f9e7147dcd5c22526f5c3a180252
dewanshDT <dewansh.dt@gmail.com>
2023-08-28 09:10:44 +0530

    remove imports

--- client/modules/IDE/hooks/useWhatPage.js
+++ client/modules/IDE/hooks/useWhatPage.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
