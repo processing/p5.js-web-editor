1b1c02285d64b41f3e9fb758e7fc53422f0e7f73
chiranjeev kundu <chiranjeevkundu2000@gmail.com>
2023-09-12 12:07:08 +0530

    changes requested

--- client/modules/IDE/components/Searchbar/Searchbar.jsx
+++ client/modules/IDE/components/Searchbar/Searchbar.jsx
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { throttle } from 'lodash';
-import { withTranslation } from 'react-i18next';
-import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
 import SearchIcon from '../../../../images/magnifyingglass.svg';
 
 const Searchbar = ({
   searchTerm,
   setSearchTerm,
   resetSearchTerm,
-  searchLabel,
-  t
+  searchLabel
 }) => {
   const [searchValue, setSearchValue] = useState(searchTerm);
+  const { t } = useTranslation(); // Use useTranslation to access t
 
   const searchChange = () => {
     setSearchTerm(searchValue.trim());
   };
 
-  const throttledSearchChange = throttle(searchChange, 500);
+  const throttledSearchChange = useMemo(() => throttle(searchChange, 500), [
+    searchChange
+  ]);
 
   useEffect(
     () => () => {
@@ -36,18 +37,6 @@ const Searchbar = ({
     const newValue = e.target.value;
     setSearchValue(newValue);
     throttledSearchChange(newValue.trim());
-  };
-
-  Searchbar.propTypes = {
-    searchTerm: PropTypes.string.isRequired,
-    setSearchTerm: PropTypes.func.isRequired,
-    resetSearchTerm: PropTypes.func.isRequired,
-    searchLabel: PropTypes.string,
-    t: PropTypes.func.isRequired
-  };
-
-  Searchbar.defaultProps = {
-    searchLabel: i18next.t('Searchbar.SearchSketch')
   };
 
   return (
@@ -75,4 +64,15 @@ const Searchbar = ({
   );
 };
 
-export default withTranslation()(Searchbar);
+Searchbar.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  setSearchTerm: PropTypes.func.isRequired,
+  resetSearchTerm: PropTypes.func.isRequired,
+  searchLabel: PropTypes.string
+};
+
+Searchbar.defaultProps = {
+  searchLabel: 'Default Search Label'
+};
+
+export default Searchbar;
