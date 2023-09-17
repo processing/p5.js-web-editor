e18009a5e13425bb0abc6858693705aed60c87e1
Linda Paiste <lindapaiste@gmail.com>
2023-09-17 16:58:59 -0500

    Apply new responsive styles

--- client/components/Dropdown/TableDropdown.jsx
+++ client/components/Dropdown/TableDropdown.jsx
@@ -1,11 +1,39 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import styled from 'styled-components';
 import { prop, remSize } from '../../theme';
 import DropdownMenu from './DropdownMenu';
 
-const TableDropdown = styled(DropdownMenu).attrs({ align: 'right' })`
+import DownFilledTriangleIcon from '../../images/down-filled-triangle.svg';
+import MoreIconSvg from '../../images/more.svg';
+
+const DotsHorizontal = styled(MoreIconSvg)`
+  transform: rotate(90deg);
+`;
+
+const TableDropdownIcon = () => {
+  // TODO: centralize breakpoints
+  const isMobile = useMediaQuery({ maxWidth: 770 });
+
+  return isMobile ? (
+    <DotsHorizontal focusable="false" aria-hidden="true" />
+  ) : (
+    <DownFilledTriangleIcon focusable="false" aria-hidden="true" />
+  );
+};
+
+const TableDropdown = styled(DropdownMenu).attrs({
+  align: 'right',
+  anchor: <TableDropdownIcon />
+})`
   & > button {
     width: ${remSize(25)};
     height: ${remSize(25)};
+    padding: 0;
+    & svg {
+      max-width: 100%;
+      max-height: 100%;
+    }
     & polygon,
     & path {
       fill: ${prop('inactiveTextColor')};
