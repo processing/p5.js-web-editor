6798a87b806ca698bea45bf35e559e6c6d40cb05
Cecilia Hollins <hollins.cecilia@gmail.com>
2023-10-03 07:42:04 +1100

    adding instructions view

---
+++ client/modules/IDE/pages/PuzzleView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PuzzleView = () => (
+  <div className="puzzle-view-container">
+    <div className="content">
+      <h1> Hello World! </h1>
+    </div>
+  </div>
+);
+
+export default PuzzleView;
