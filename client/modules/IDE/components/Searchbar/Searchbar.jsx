25af8eb85763a74b8b97d1d5a484abddb9b0e141
Karanpal@97 <karangill1810@gmail.com>
2023-10-17 18:28:47 +0530

    Moved searchbar to a functional component

--- client/modules/IDE/components/Searchbar/Searchbar.jsx
+++ client/modules/IDE/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/display-name */
-
 import React, { useState, useEffect, useCallback } from 'react';
 import { throttle } from 'lodash';
 import { withTranslation } from 'react-i18next';
