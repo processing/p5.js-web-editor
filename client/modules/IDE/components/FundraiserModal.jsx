2b286ca265e31bb18de47e7dbb51719d267a8eaf
Rachel Lim <rachellim@Rachels-MacBook-Pro.local>
2023-12-13 09:13:49 +0900

    add pf logo and update cta styling

--- client/modules/IDE/components/FundraiserModal.jsx
+++ client/modules/IDE/components/FundraiserModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import SquareLogoIcon from '../../../images/p5js-square-logo.svg';
+import LogoIcon from '../../../images/p5js-logo.svg';
+import PFLogoIcon from '../../../images/processing-foundation-logo.svg';
 
 export default function FundraiserModal() {
   const { t } = useTranslation();
@@ -8,13 +9,28 @@ export default function FundraiserModal() {
   return (
     <div className="fundraiser">
       <p className="fundraiser__description">{t('Fundraiser.Description')}</p>
-      <SquareLogoIcon
-        className="about__logo"
-        role="img"
-        aria-label={t('Common.p5logoARIA')}
-        focusable="false"
-      />
-      <p className="fundraiser__description">{t('Fundraiser.CallToAction')}</p>
+      <div className="fundraiser__img-container">
+        <LogoIcon
+          className="fundraiser__logo"
+          role="img"
+          aria-label={t('Common.p5logoARIA')}
+          focusable="false"
+        />
+        <PFLogoIcon
+          className="fundraiser__logo"
+          role="img"
+          aria-label={t('Common.p5logoARIA')}
+          focusable="false"
+        />
+      </div>
+      <a
+        className="fundraiser__CTA"
+        href="https://donorbox.org/to-the-power-of-10"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {t('Fundraiser.CallToAction')}
+      </a>
     </div>
   );
 }
