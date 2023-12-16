42dc6851a704dcb26dc2e5ca4dd16ab148734c5e
mhsh312 <mahesh.khatri.0312@gmail.com>
2023-12-17 01:35:28 +0530

    frontend changes

--- client/modules/User/components/CollectionShareButton.jsx
+++ client/modules/User/components/CollectionShareButton.jsx
@@ -6,13 +6,18 @@ import Button from '../../../common/Button';
 import { DropdownArrowIcon } from '../../../common/icons';
 import useModalClose from '../../../common/useModalClose';
 import CopyableInput from '../../IDE/components/CopyableInput';
+import { exportCollectionAsZip } from '../../IDE/actions/project';
 
 const ShareURL = ({ value }) => {
   const [showURL, setShowURL] = useState(false);
   const { t } = useTranslation();
   const close = useCallback(() => setShowURL(false), [setShowURL]);
   const ref = useModalClose(close);
-
+  function downloadCollection() {
+    const urlArray = value.split('/');
+    const collectionId = urlArray[urlArray.length - 1];
+    exportCollectionAsZip(collectionId);
+  }
   return (
     <div className="collection-share" ref={ref}>
       <Button
@@ -24,6 +29,9 @@ const ShareURL = ({ value }) => {
       {showURL && (
         <div className="collection__share-dropdown">
           <CopyableInput value={value} label={t('Collection.URLLink')} />
+          <Button onClick={downloadCollection}>
+            {t('Collection.Download')}
+          </Button>
         </div>
       )}
     </div>
