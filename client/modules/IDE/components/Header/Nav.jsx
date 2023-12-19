c1fc9725036a35ac0af241b9519f7fe7e02aa745
Arpit Kandwal <arpit.kandwal2004@gmail.com>
2023-12-19 13:12:59 +0530

    Added route to p5js.org Conveniently switch back to the p5js.org website from the web editor page #2742

--- client/modules/IDE/components/Header/Nav.jsx
+++ client/modules/IDE/components/Header/Nav.jsx
@@ -111,12 +111,14 @@ const DashboardMenu = () => {
   return (
     <ul className="nav__items-left">
       <li className="nav__item-logo">
-        <LogoIcon
-          role="img"
-          aria-label={t('Common.p5logoARIA')}
-          focusable="false"
-          className="svg__logo"
-        />
+        <a href="https://p5js.org">
+          <LogoIcon
+            role="img"
+            aria-label={t('Common.p5logoARIA')}
+            focusable="false"
+            className="svg__logo"
+          />
+        </a>
       </li>
       <li className="nav__item nav__item--no-icon">
         <Link to={editorLink} className="nav__back-link">
@@ -159,12 +161,14 @@ const ProjectMenu = () => {
   return (
     <ul className="nav__items-left">
       <li className="nav__item-logo">
-        <LogoIcon
-          role="img"
-          aria-label={t('Common.p5logoARIA')}
-          focusable="false"
-          className="svg__logo"
-        />
+        <a href="https://p5js.org">
+          <LogoIcon
+            role="img"
+            aria-label={t('Common.p5logoARIA')}
+            focusable="false"
+            className="svg__logo"
+          />
+        </a>
       </li>
       <NavDropdownMenu id="file" title={t('Nav.File.Title')}>
         <NavMenuItem onClick={newSketch}>{t('Nav.File.New')}</NavMenuItem>
