39baabd2f52c6f35a0f58b2258bb2aaa3dd88938
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2023-12-24 01:33:50 +0530

    Convert NavBar class component to functional component

--- client/components/NavBasic.jsx
+++ client/components/NavBasic.jsx
@@ -1,52 +1,46 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef } from 'react';
 import { withTranslation } from 'react-i18next';
 
 import LogoIcon from '../images/p5js-logo-small.svg';
 import ArrowIcon from '../images/triangle-arrow-left.svg';
 
-class NavBasic extends React.PureComponent {
-  static defaultProps = {
-    onBack: null
-  };
+const NavBasic = ({ onBack, t }) => {
+  const nodeRef = useRef(null);
 
-  render() {
-    return (
-      <nav
-        className="nav"
-        title="main-navigation"
-        ref={(node) => {
-          this.node = node;
-        }}
-      >
-        <ul className="nav__items-left">
-          <li className="nav__item-logo">
-            <LogoIcon
-              role="img"
-              aria-label={this.props.t('Common.p5logoARIA')}
-              focusable="false"
-              className="svg__logo"
-            />
+  return (
+    <nav className="nav" title="main-navigation" ref={nodeRef}>
+      <ul className="nav__items-left">
+        <li className="nav__item-logo">
+          <LogoIcon
+            role="img"
+            aria-label={t('Common.p5logoARIA')}
+            focusable="false"
+            className="svg__logo"
+          />
+        </li>
+        {onBack && (
+          <li className="nav__item">
+            <button onClick={onBack}>
+              <span className="nav__item-header">
+                <ArrowIcon focusable="false" aria-hidden="true" />
+              </span>
+              Back to the editor
+            </button>
           </li>
-          {this.props.onBack && (
-            <li className="nav__item">
-              <button onClick={this.props.onBack}>
-                <span className="nav__item-header">
-                  <ArrowIcon focusable="false" aria-hidden="true" />
-                </span>
-                Back to the editor
-              </button>
-            </li>
-          )}
-        </ul>
-      </nav>
-    );
-  }
-}
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 NavBasic.propTypes = {
   onBack: PropTypes.func,
   t: PropTypes.func.isRequired
+};
+
+NavBasic.defaultProps = {
+  onBack: null
 };
 
 export default withTranslation()(NavBasic);
