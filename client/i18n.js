4397213cfda235844ee8f325b859ad9b71027cca
abedin-ashraf <nurulabedinashraf@gmail.com>
2024-01-06 00:38:51 -0600

    Changed i18n.js to translate Bengali

--- client/i18n.js
+++ client/i18n.js
@@ -42,7 +42,7 @@ export const availableLanguages = [
 
 export function languageKeyToLabel(lang) {
   const languageMap = {
-    be: 'Bengali',
+    be: 'বাংলা',
     de: 'Deutsch',
     'en-US': 'English',
     'es-419': 'Español',
