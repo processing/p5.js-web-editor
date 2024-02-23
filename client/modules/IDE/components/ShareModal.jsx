12f26170e3c5376c5b4aef15d5801cd161ce6fa9
Danish <md236572@gmail.com>
2024-02-23 12:27:54 +0530

    Added Edit Slug option when the user open the share modal

--- client/modules/IDE/components/ShareModal.jsx
+++ client/modules/IDE/components/ShareModal.jsx
@@ -1,13 +1,29 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import CopyableInput from './CopyableInput';
-// import getConfig from '../../../utils/getConfig';
 
 const ShareModal = ({ projectId, ownerUsername, projectName }) => {
   const { t } = useTranslation();
   const hostname = window.location.origin;
-  // const previewUrl = getConfig('PREVIEW_URL');
+  const [slug, setSlug] = useState(
+    projectName.toLowerCase().replace(/\s+/g, '-')
+  ); // Initialize slug with project name
+
+  const handleSlugChange = (event) => {
+    setSlug(event.target.value);
+  };
+
+  const handleEditSlug = () => {
+    const newUrl = `${hostname}/${ownerUsername}/sketches/${slug}`;
+
+    window.history.pushState({}, '', newUrl);
+
+    // Provide feedback to the user
+    // use alerts, notifications, or a message within the modal
+    alert('Slug updated successfully!');
+  };
+
   return (
     <div className="share-modal">
       <h3 className="share-modal__project-name">{projectName}</h3>
@@ -15,22 +31,16 @@ const ShareModal = ({ projectId, ownerUsername, projectName }) => {
         label={t('ShareModal.Embed')}
         value={`<iframe src="${hostname}/${ownerUsername}/full/${projectId}"></iframe>`}
       />
-      {/* CAT removing due to phishing issues */}
-      {/* <CopyableInput
-        label={t('ShareModal.Present')}
-        hasPreviewLink
-        value={`${previewUrl}/${ownerUsername}/present/${projectId}`}
-      /> */}
       <CopyableInput
         label={t('ShareModal.Fullscreen')}
         hasPreviewLink
         value={`${hostname}/${ownerUsername}/full/${projectId}`}
       />
-      <CopyableInput
-        label={t('ShareModal.Edit')}
-        hasPreviewLink
-        value={`${hostname}/${ownerUsername}/sketches/${projectId}`}
-      />
+      <div className="edit-slug">
+        <label htmlFor="slug">{t('ShareModal.EditSlug')}</label>
+        <input type="text" id="slug" value={slug} onChange={handleSlugChange} />
+        <button onClick={handleEditSlug}>{t('ShareModal.SaveSlug')}</button>
+      </div>
     </div>
   );
 };
