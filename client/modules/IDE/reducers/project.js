203122f07777a502fe10108b0ea1bec19c2c2b49
Piyush <chandra.piyush17@gmail.com>
2024-05-16 00:49:25 +0530

    refactor project reducers and actions using redux toolkit

--- client/modules/IDE/reducers/project.js
+++ client/modules/IDE/reducers/project.js
@@ -1,4 +1,4 @@
-import * as ActionTypes from '../../../constants';
+import { createSlice } from '@reduxjs/toolkit';
 import { generateProjectName } from '../../../utils/generateRandomName';
 
 const initialState = () => {
@@ -12,40 +12,52 @@ const initialState = () => {
   };
 };
 
-const project = (state, action) => {
-  if (state === undefined) {
-    state = initialState(); // eslint-disable-line
+const projectSlice = createSlice({
+  name: 'project',
+  initialState,
+  reducers: {
+    setProjectName(state, action) {
+      state.name = action.payload;
+    },
+    newProject(state, action) {
+      const { id, name, updatedAt, owner } = action.payload;
+      state.id = id;
+      state.name = name;
+      state.updatedAt = updatedAt;
+      state.owner = owner;
+      state.isSaving = false;
+    },
+    setProject(state, action) {
+      const { id, name, updatedAt, owner } = action.payload;
+      state.id = id;
+      state.name = name;
+      state.updatedAt = updatedAt;
+      state.owner = owner;
+      state.isSaving = false;
+    },
+    resetProject(state) {
+      Object.assign(state, initialState);
+    },
+    setProjectSavedTime(state, action) {
+      state.updatedAt = action.payload;
+    },
+    startSavingProject(state) {
+      state.isSaving = true;
+    },
+    endSavingProject(state) {
+      state.isSaving = false;
+    }
   }
-  switch (action.type) {
-    case ActionTypes.SET_PROJECT_NAME:
-      return Object.assign({}, { ...state }, { name: action.name });
-    case ActionTypes.NEW_PROJECT:
-      return {
-        id: action.project.id,
-        name: action.project.name,
-        updatedAt: action.project.updatedAt,
-        owner: action.owner,
-        isSaving: false
-      };
-    case ActionTypes.SET_PROJECT:
-      return {
-        id: action.project.id,
-        name: action.project.name,
-        updatedAt: action.project.updatedAt,
-        owner: action.owner,
-        isSaving: false
-      };
-    case ActionTypes.RESET_PROJECT:
-      return initialState();
-    case ActionTypes.SET_PROJECT_SAVED_TIME:
-      return Object.assign({}, state, { updatedAt: action.value });
-    case ActionTypes.START_SAVING_PROJECT:
-      return Object.assign({}, state, { isSaving: true });
-    case ActionTypes.END_SAVING_PROJECT:
-      return Object.assign({}, state, { isSaving: false });
-    default:
-      return state;
-  }
-};
+});
 
-export default project;
+export const {
+  setProjectName,
+  newProject,
+  setProject,
+  resetProject,
+  setProjectSavedTime,
+  startSavingProject,
+  endSavingProject
+} = projectSlice.actions;
+
+export default projectSlice.reducer;
