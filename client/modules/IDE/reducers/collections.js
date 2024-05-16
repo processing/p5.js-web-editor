2c887c547af25d043f997a882cd3ecba4b0c047d
Piyush <chandra.piyush17@gmail.com>
2024-05-16 20:46:05 +0530

    refactor collections reducers and actions using redux toolkit

--- client/modules/IDE/reducers/collections.js
+++ client/modules/IDE/reducers/collections.js
@@ -1,28 +1,27 @@
-import * as ActionTypes from '../../../constants';
+import { createSlice } from '@reduxjs/toolkit';
 
-const sketches = (state = [], action) => {
-  switch (action.type) {
-    case ActionTypes.SET_COLLECTIONS:
-      return action.collections;
-
-    case ActionTypes.DELETE_COLLECTION:
-      return state.filter(({ id }) => action.collectionId !== id);
-
-    // The API returns the complete new edited collection
-    // with any items added or removed
-    case ActionTypes.EDIT_COLLECTION:
-    case ActionTypes.ADD_TO_COLLECTION:
-    case ActionTypes.REMOVE_FROM_COLLECTION:
-      return state.map((collection) => {
-        if (collection.id === action.payload.id) {
-          return action.payload;
-        }
-
-        return collection;
-      });
-    default:
-      return state;
+const sketchesSlice = createSlice({
+  name: 'sketches',
+  initialState: [],
+  reducers: {
+    setCollections: (state, action) => action.payload,
+    delCollection: (state, action) => {
+      const { collectionId } = action.payload;
+      return state.filter((collection) => collection.id !== collectionId);
+    },
+    updateCollection: (state, action) => {
+      const updatedCollection = action.payload;
+      return state.map((collection) =>
+        collection.id === updatedCollection.id ? updatedCollection : collection
+      );
+    }
   }
-};
+});
 
-export default sketches;
+export const {
+  setCollections,
+  delCollection,
+  updateCollection
+} = sketchesSlice.actions;
+
+export default sketchesSlice.reducer;
