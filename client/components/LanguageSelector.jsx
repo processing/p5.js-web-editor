1c663b11ec5f02bdd6d749ce54f9151a7ea1afae
Kavya <kavyarathod0507@gmail.com>
2024-08-08 12:11:56 +0530

    english langauge preferred

---
+++ client/components/LanguageSelector.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+  const languages = [
+    'be',
+    'de',
+    'en-US',
+    'es-419',
+    'fr-CA',
+    'hi',
+    'it',
+    'ja',
+    'ko',
+    'pt-BR',
+    'sv',
+    'uk-UA',
+    'zh-CN',
+    'zh-TW',
+    'tr',
+    'ur'
+  ];
+
+  // Ensure English (en-US) is always at the top
+  // eslint-disable-next-line prettier/prettier
+  const sortedLanguages = ['en-US', ...languages.filter(lang => lang !== 'en-US')];
+
+  const handleLanguageChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
+  return (
+    <div className="language-selector">
+      <label htmlFor="language-select">Select Language: </label>
+      <select
+        id="language-select"
+        onChange={handleLanguageChange}
+        value={i18n.language}
+      >
+        {sortedLanguages.map((lang) => (
+          <option key={lang} value={lang}>
+            {lang.toUpperCase()}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default LanguageSelector;
