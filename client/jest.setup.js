35c8adac326f6d53b461bcd05ea4a2a94d08e7e9
JATIN <Jatin04072005@gmail.com>
2026-04-06 13:34:54 +0530

    hidding  unwanted/Not-Usefull error  of react

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -5,3 +5,33 @@ import 'regenerator-runtime/runtime';
 // See: https://github.com/testing-library/jest-dom
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom';
+
+const originalConsoleError = console.error;
+const originalConsoleWarn = console.warn;
+
+console.error = (...args) => {
+  // Use .join() to check the entire message content regardless of how React formats it
+  const fullMessage = args.join(' ');
+
+  if (
+    fullMessage.includes(
+      'The prop `component` is marked as required in `Route`'
+    )
+  ) {
+    return;
+  }
+  originalConsoleError.apply(console, args);
+};
+
+console.warn = (...args) => {
+  const fullMessage = args.join(' ');
+
+  if (
+    fullMessage.includes('has been renamed') ||
+    fullMessage.includes('SideEffect(NullComponent)') ||
+    fullMessage.includes('Duplicate schema index')
+  ) {
+    return;
+  }
+  originalConsoleWarn.apply(console, args);
+};
