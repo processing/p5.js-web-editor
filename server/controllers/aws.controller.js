784268ab605a3cd80fd55eaa492a25504e78ee17
nityam <nixxspeaks@gmail.com>
2026-05-29 12:40:20 +0530

    count only referenced assets toward upload limit

--- server/controllers/aws.controller.js
+++ server/controllers/aws.controller.js
@@ -105,7 +105,6 @@ export async function listObjectsInS3ForUser(userId) {
         size: asset.size,
         url: `${process.env.S3_BUCKET_URL_BASE}${asset.key}`
       };
-      totalSize += asset.size;
 
       const wasMatched = projects.some((project) =>
         project.files.some((file) => {
@@ -121,8 +120,11 @@ export async function listObjectsInS3ForUser(userId) {
         })
       );
 
+      // Only count referenced assets toward the limit; unreferenced S3
+      // leftovers aren't shown or deletable in "My Assets".
       if (wasMatched) {
         projectAssets.push(foundAsset);
+        totalSize += asset.size;
       }
     });
 
