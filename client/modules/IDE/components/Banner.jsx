2c6819935a22bb5b04060d329297d76c49f44ade
raclim <43053081+raclim@users.noreply.github.com>
2026-07-13 13:27:05 -0400

    update banner copy and translations, set banner on

--- client/modules/IDE/components/Banner.jsx
+++ client/modules/IDE/components/Banner.jsx
@@ -24,7 +24,7 @@ import { CrossIcon } from '../../../common/icons';
 
 const Banner = ({ onClose }) => {
   // URL can be updated depending on the opportunity or announcement.
-  const bannerURL = 'https://processingfoundation.org/donate';
+  const bannerURL = 'https://github.com/processing/p5.js/issues/8870';
 
   // currently holds donation copy, will switch back when temp maintenance is done
   const bannerCopy = (
